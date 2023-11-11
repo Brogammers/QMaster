3972455bbdf9b3b515ba9d7b5b7d8003435f5365
Fam Awad <fam@awadlouis.com>
2023-11-11 22:22:15 +0200

    Adding User java class and imports

---
+++ User.java
@@ -0,0 +1,11 @@
+import java.security.SecureRandom;
+import java.security.spec.KeySpec;
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.PBEKeySpec;
+
+public class User {
+
+    private static byte[] getSecureHash(String s) {
+        return null;
+    }
+}
