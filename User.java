6c8475421d821afb9bc918149a967a9e123b3e85
Fam Awad <fam@awadlouis.com>
2023-11-11 22:33:58 +0200

    Creating the getSecureHash() function

--- User.java
+++ User.java
@@ -1,11 +1,21 @@
-import java.security.SecureRandom;
 import java.security.spec.KeySpec;
+import java.time.LocalDate;
 import javax.crypto.SecretKeyFactory;
 import javax.crypto.spec.PBEKeySpec;
 
 public class User {
 
     private static byte[] getSecureHash(String s) {
-        return null;
+        KeySpec spec = new PBEKeySpec(s.toCharArray(),
+                Salts.getSaltByDay(LocalDate.now().getDayOfWeek().getValue() - 1),
+                94155, 265); // Gets hashing key using day of the week
+        byte[] hash = null;
+        try {
+            SecretKeyFactory factory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA1"); // Hashing algorithm
+            hash = factory.generateSecret(spec).getEncoded();
+            return hash; // Hash was successful
+        } catch (Exception e) {
+            return null;
+        }
     }
 }
