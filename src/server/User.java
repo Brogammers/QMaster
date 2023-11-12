13f21a7f4b58797b3b192639ae42c1d8ec85f346
Fam Awad <fam@awadlouis.com>
2023-11-12 19:37:57 +0200

    Fixing spacing

--- src/server/User.java
+++ src/server/User.java
@@ -14,13 +14,12 @@ public class User {
   private byte[] email;
 
   public User(
-    String name,
-    Date dateOfRegistration,
-    Date dateOfBirth,
-    String countryOfOrigin,
-    String password,
-    String email
-  ) {
+      String name,
+      Date dateOfRegistration,
+      Date dateOfBirth,
+      String countryOfOrigin,
+      String password,
+      String email) {
     this.name = name;
     this.dateOfRegistration = dateOfRegistration;
     this.dateOfBirth = dateOfBirth;
@@ -31,16 +30,14 @@ public class User {
 
   private static byte[] getSecureHash(String s) {
     KeySpec spec = new PBEKeySpec(
-      s.toCharArray(),
-      Salts.getSaltByDay(LocalDate.now().getDayOfWeek().getValue() - 1),
-      141551,
-      265
-    ); // Gets hashing key using day of the week
+        s.toCharArray(),
+        Salts.getSaltByDay(LocalDate.now().getDayOfWeek().getValue() - 1),
+        141551,
+        265); // Gets hashing key using day of the week
     byte[] hash = null;
     try {
       SecretKeyFactory factory = SecretKeyFactory.getInstance(
-        "PBKDF2WithHmacSHA1"
-      ); // Hashing algorithm
+          "PBKDF2WithHmacSHA1"); // Hashing algorithm
       hash = factory.generateSecret(spec).getEncoded();
       return hash; // Hash was successful
     } catch (Exception e) {
