9599f3a2bc6fb4b1c92d014ab12ef15d7e006752
FamALouiz <fam@awadlouis.com>
2023-11-15 19:37:00 +0200

    Creating the loadUserByUsername method

--- server/app/que/src/main/java/com/que/que/User/AppUserService.java
+++ server/app/que/src/main/java/com/que/que/User/AppUserService.java
@@ -3,13 +3,20 @@ package com.que.que.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import lombok.AllArgsConstructor;
 
+@Service
+@AllArgsConstructor
 public class AppUserService implements UserDetailsService {
+
+    private final AppUserRepository appUserRepository;
+    private final static String USER_NOT_FOUND_MSG = "User with email %s was not found in the database";
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        // TODO Auto-generated method stub
-        return null;
+        return appUserRepository.findByEmail(username)
+                .orElseThrow(() -> new UsernameNotFoundException(String.format(USER_NOT_FOUND_MSG, username)));
     }
 
 }
