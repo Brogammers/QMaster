22cca286a59f92cfec2a35061aab394bff689292
FamALouiz <fam@awadlouis.com>
2023-11-15 19:00:27 +0200

    Creating initial que backend app

---
+++ server/app/que/src/main/java/com/que/que/QueApplication.java
@@ -0,0 +1,13 @@
+package com.que.que;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QueApplication.class, args);
+	}
+
+}
