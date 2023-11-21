193ec3f1301bf8ad86abeb4389872ab6bacee9c9
Hatem Soliman <hatemthedev@gmail.com>
2023-11-21 11:36:29 +0200

    Installed a few devDependencies and initialized NPM

--- mobile-app/app/_layout.tsx
+++ mobile-app/app/_layout.tsx
@@ -4,6 +4,7 @@ import { useFonts } from 'expo-font';
 import { SplashScreen, Stack } from 'expo-router';
 import { useEffect } from 'react';
 import { useColorScheme } from 'react-native';
+import 'react-native-gesture-handler';
 
 export {
   // Catch any errors thrown by the Layout component.
