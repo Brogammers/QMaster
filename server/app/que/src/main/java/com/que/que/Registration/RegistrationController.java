26148afb248a8965f4c68aa6664e9a83a5341e96
FamALouiz <88089745+FamALouiz@users.noreply.github.com>
2023-11-25 10:14:18 +0200

    Update RegistrationController.java
    Specifying that the endpoints will be changed later on
    
    Signed-off-by: FamALouiz <88089745+FamALouiz@users.noreply.github.com>

--- server/app/que/src/main/java/com/que/que/Registration/RegistrationController.java
+++ server/app/que/src/main/java/com/que/que/Registration/RegistrationController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+// This endpoint is not set yet. It was used in the example and will be changed later on.
 @RequestMapping(path = "api/v1/registration")
 @AllArgsConstructor
 public class RegistrationController {
