8e909843166c58a581c9eaf936c97c9934ec0ff9
Fam Awad <fam@awadlouis.com>
2023-11-26 11:28:46 +0200

    Adding one to one linkage

--- server/app/que/src/main/java/com/que/que/Login/LoginEntry.java
+++ server/app/que/src/main/java/com/que/que/Login/LoginEntry.java
@@ -6,6 +6,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.SequenceGenerator;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -21,8 +22,8 @@ public class LoginEntry {
   @SequenceGenerator(name = "login_entry_sequence", sequenceName = "login_entry_sequence", allocationSize = 1)
   @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "login_entry_sequence")
   private Long id;
-  @Column(nullable = false)
-  @JoinColumn(name = "app_user_email")
+  @OneToOne
+  @JoinColumn(nullable = false, name = "app_user_email")
   private String email;
   @Column(nullable = false)
   private String password;
