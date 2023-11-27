8b84ce18d69590bcc41a4ac77b55e9777953b1aa
Mohammed Elazab <mohammedelazab16@gmail.com>
2023-11-27 21:58:48 +0200

    authentication

--- mobile-app/app/googleAuth.tsx
+++ mobile-app/app/googleAuth.tsx
@@ -10,45 +10,50 @@ WebBrowser.maybeCompleteAuthSession();
 const GoogleAuth = () => {
   const [userInfo, setUserInfo] = React.useState(null)
   const [request, response, promptAsync] = Google.useAuthRequest({
-    androidClientId: '417915066205-5ttil2s76tgfahtmvn3ltvgl98vuntuk.apps.googleusercontent.com',
-    iosClientId: '417915066205-eo3sb2te9mdkp4a1et2j4pmm4i559chh.apps.googleusercontent.com',
-    
-  })
+    androidClientId: '417915066205-dcpj6gokigcfhpf7l9oqu47m5db8esdu.apps.googleusercontent.com',
+    iosClientId: '417915066205-rabucr37ts892rl4cb6n2bg9t8b4010o.apps.googleusercontent.com',
+    webClientId: '417915066205-0p5j27kr3ng63n917a0il2rv7aml49dg.apps.googleusercontent.com',
+  }
+  )
+
 
   React.useEffect(() => {
     handleSignInWithGoogle()
   },[response])
   
-  const getUserInfo = async (token: any) => {
-    if (!token) return ;
-    try{
-      const response = await fetch (
-        'https://www.googleapis.com/userinfo/v2/me',
-        {
-          headers: {Authorization: `Bearer ${token}`},
-        }
-      )
-
-      const user = await response.json();
-      await AsyncStorage.setItem('@user', JSON.stringify(user));
-      setUserInfo(user);
-    } catch(error) {
-      alert('something went wrong')
-    };
-  }
-
+  
   async function handleSignInWithGoogle() {
     const user = await AsyncStorage.getItem('@user');
     if (!user) {
-      if(response?.type === "success"){
-      await getUserInfo(response.authentication?.accessToken);
-    }
-    }
-    else {
-      setUserInfo(JSON.parse(user))
+      try {
+        if (response?.type === 'success') {
+          await getUserInfo(response.authentication?.accessToken);
+        }
+      } catch (error) {
+        console.error('Error handling Google Sign-In:', error);
+      }
+    } else {
+      setUserInfo(JSON.parse(user));
     }
   }
+  
+  const getUserInfo = async (token: string | undefined) => {
+    if (!token) return;
+    try {
+      const response = await fetch(
+        "https://www.googleapis.com/userinfo/v2/me",
+        {
+          headers: { Authorization: `Bearer ${token}` },
+        }
+      );
 
+      const user = await response.json();
+      await AsyncStorage.setItem("@user", JSON.stringify(user));
+      setUserInfo(user);
+    } catch (error) {
+      console.error('Error fetching user info:', error)
+    }
+  };
 
   return (
     <View className='flex items-center justify-center h-screen'>
