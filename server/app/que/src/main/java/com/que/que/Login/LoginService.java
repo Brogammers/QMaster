39f1fb054a29e48b74d4c3d62738c9d415e8bfaa
OSafwat <omarsafsab2004@gmail.com>
2023-11-30 18:49:57 +0200

    QUEUE-89 finished user login service checkIfValidUser and fixed EmailValidator email checker function

--- server/app/que/src/main/java/com/que/que/Login/LoginService.java
+++ server/app/que/src/main/java/com/que/que/Login/LoginService.java
@@ -1,17 +1,25 @@
 package com.que.que.Login;
 
-import org.springframework.stereotype.Service;
+import com.que.que.Registration.EmailValidator;
+import com.que.que.User.AppUser;
 import com.que.que.User.AppUserRepository;
 import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
 
 @Service
 @AllArgsConstructor
 public class LoginService {
+
   private final AppUserRepository appUserRepository;
+  private final EmailValidator emailValidator;
 
   private boolean checkIfValidUser(String email, String password) {
-    // TODO
-    return false;
+    boolean isValidEmail = emailValidator.test(email);
+    if (!isValidEmail) return false;
+    AppUser user = appUserRepository
+      .findByEmail(email)
+      .orElseThrow(() -> new IllegalStateException("User not found."));
+    return user.isEnabled();
   }
 
   public void loginUser(String email, String password) {
--- server/app/que/src/main/java/com/que/que/Registration/EmailValidator.java
+++ server/app/que/src/main/java/com/que/que/Registration/EmailValidator.java
@@ -1,7 +1,6 @@
 package com.que.que.Registration;
 
 import java.util.function.Predicate;
-
 import org.springframework.stereotype.Service;
 
 @Service
@@ -9,6 +8,14 @@ public class EmailValidator implements Predicate<String> {
 
   @Override
   public boolean test(String s) {
+    if (
+      s.charAt(0) == '@' || s.length() <= 5 || s.charAt(0) == '.'
+    ) return false;
+    if (!s.contains("@") || !s.contains(".com")) return false;
+    if (
+      s.indexOf("@") > s.indexOf(".com") ||
+      Math.abs(s.indexOf("@") - s.indexOf(".com")) == 1
+    ) return false;
     return true;
   }
 }
