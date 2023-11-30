d93832b712d9baee732716f9b31d628214f43373
FamALouiz <fam@awadlouis.com>
2023-11-30 18:35:13 +0200

    Adding registration url

--- server/app/que/src/main/java/com/que/que/Registration/RegistrationController.java
+++ server/app/que/src/main/java/com/que/que/Registration/RegistrationController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 // This endpoint is not set yet. It was used in the example and will be changed
 // later on.
+// splandamedia.com/api/v1/registration
 @RequestMapping(path = "api/v1/registration")
 @AllArgsConstructor
 public class RegistrationController {
