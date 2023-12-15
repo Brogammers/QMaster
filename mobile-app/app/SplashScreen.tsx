5ae101869cba0b430f0168783a090f4fb0b28553
Mohammed Elazab <mohammedelazab16@gmail.com>
2023-12-15 16:50:22 +0200

    animated splash screen

---
+++ mobile-app/app/SplashScreen.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
+import LottieView from 'lottie-react-native';
+import Logo from '@/assets/images/splash.json';
+
+export default function Onboarding() {
+  return (
+    <View className='items-center justify-center h-screen bg-ocean-blue' style={styles.container}>
+      <LottieView
+        source={Logo}
+        autoPlay
+        loop
+      />
+    </View>
+  )
+}
+
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#17222D',
+    color: '#FFF',
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  row: {
+    width: '100%',
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
