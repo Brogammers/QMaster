76b365b7e78bc703ee0d461f9b13fe0ae2597df5
FamALouiz <fam@awadlouis.com>
2023-12-19 16:37:07 +0100

    Removing unused imports

--- server/app/que/src/main/java/com/que/que/Login/LoginController.java
+++ server/app/que/src/main/java/com/que/que/Login/LoginController.java
@@ -1,10 +1,8 @@
 package com.que.que.Login;
 
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import lombok.AllArgsConstructor;
