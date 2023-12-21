dcafbf0696e8b27807323bb530a9fd61986545b5
FamALouiz <fam@awadlouis.com>
2023-12-21 21:32:56 +0100

    Adding endpoint for queue creation

--- server/app/que/src/main/java/com/que/que/Queue/QueueController.java
+++ server/app/que/src/main/java/com/que/que/Queue/QueueController.java
@@ -1,4 +1,22 @@
 package com.que.que.Queue;
 
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.AllArgsConstructor;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@RestController
+@RequestMapping(path = "api/v1/queue")
+@AllArgsConstructor
 public class QueueController {
+
+    private final QueueService queueService;
+
+    @PostMapping("create")
+    public void createNewQueue(@RequestParam("id") Long id) {
+        queueService.createNewQueue(id);
+    }
 }
