f6725e6b1e39c998c39cba096bbc70f3bfbe8f0c
FamALouiz <fam@awadlouis.com>
2024-01-11 21:50:26 +0100

    Migrating to Security 6.0+

---
+++ server/app/que/src/main/java/com/que/que/Security/Config/CsrfTokenResponseHeaderBindingFilter.java
@@ -0,0 +1,32 @@
+package com.que.que.Security.Config;
+
+import java.io.IOException;
+
+import org.springframework.security.web.csrf.CsrfToken;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class CsrfTokenResponseHeaderBindingFilter extends OncePerRequestFilter {
+    protected static final String REQUEST_ATTRIBUTE_NAME = "_csrf";
+    protected static final String RESPONSE_HEADER_NAME = "X-CSRF-HEADER";
+    protected static final String RESPONSE_PARAM_NAME = "X-CSRF-PARAM";
+    protected static final String RESPONSE_TOKEN_NAME = "X-CSRF-TOKEN";
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
+            FilterChain filterChain) throws ServletException, IOException {
+        CsrfToken token = (CsrfToken) request.getAttribute(REQUEST_ATTRIBUTE_NAME);
+
+        if (token != null) {
+            response.setHeader(RESPONSE_HEADER_NAME, token.getHeaderName());
+            response.setHeader(RESPONSE_PARAM_NAME, token.getParameterName());
+            response.setHeader(RESPONSE_TOKEN_NAME, token.getToken());
+        }
+
+        filterChain.doFilter(request, response);
+    }
+}
--- server/app/que/src/main/java/com/que/que/Security/Config/WebSecurityConfig.java
+++ server/app/que/src/main/java/com/que/que/Security/Config/WebSecurityConfig.java
@@ -3,19 +3,18 @@ package com.que.que.Security.Config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.ProviderManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.csrf.HttpSessionCsrfTokenRepository;
 
-import com.que.que.User.AppUserService;
 import lombok.AllArgsConstructor;
 
 // @PreAuthorize("hasRole('ADMIN')")
@@ -25,65 +24,42 @@ import lombok.AllArgsConstructor;
 @EnableWebSecurity
 public class WebSecurityConfig {
 
-    private final AppUserService appUserService;
     private final BCryptPasswordEncoder bCryptPasswordEncoder;
 
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-
-        http
-                .authorizeHttpRequests((authz) -> {
-                    authz
-
-                            .anyRequest().permitAll();
-                }).csrf().disable();
-        return http.build();
-
-        /*
-         * http
-         * .csrf((csrf) -> csrf
-         * .csrfTokenRepository(new HttpSessionCsrfTokenRepository()))
-         * .formLogin((authz) -> {
-         * authz
-         * .loginPage("/api/v1/login").permitAll();
-         * })
-         * .logout((authz) -> {
-         * authz.permitAll();
-         * })
-         * .authorizeHttpRequests((authz) -> {
-         * authz
-         * .requestMatchers("/api/v1/registration").permitAll()
-         * .requestMatchers("/api/v1/admin/**").hasRole("ADMIN")
-         * .anyRequest().authenticated();
-         * });
-         * 
-         * return http.build();
-         */
-    }
-
-    @Bean
-    public DaoAuthenticationProvider daoAuthenticationProvider() {
-        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
-        provider.setPasswordEncoder(bCryptPasswordEncoder);
-        provider.setUserDetailsService(appUserService);
-        return provider;
-    }
-
-    @Bean
-    public AuthenticationManager authManager(HttpSecurity http) throws Exception {
-        AuthenticationManagerBuilder authenticationManagerBuilder = http
-                .getSharedObject(AuthenticationManagerBuilder.class);
-        authenticationManagerBuilder.authenticationProvider(daoAuthenticationProvider());
-        return authenticationManagerBuilder.build();
-    }
-
-    @Bean
     public InMemoryUserDetailsManager userDetailsService() {
-        UserDetails user = User.withUsername("root")
+        UserDetails admin = User.withUsername("root")
                 .password(bCryptPasswordEncoder.encode("123456789"))
                 .roles("ADMIN")
                 .build();
-        return new InMemoryUserDetailsManager(user);
+        return new InMemoryUserDetailsManager(admin);
     }
 
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        // csrfTokenRepository(CookieCsrfTokenRepository
+        // .withHttpOnlyFalse())
+
+        http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests((authorize) -> authorize
+                        .anyRequest().permitAll())
+                .httpBasic(Customizer.withDefaults())
+                .formLogin((form) -> form
+                        .loginPage("/api/v1/login"));
+        return http.build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(
+            UserDetailsService userDetailsService) {
+        DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
+        authenticationProvider.setUserDetailsService(userDetailsService);
+        authenticationProvider.setPasswordEncoder(bCryptPasswordEncoder);
+
+        ProviderManager providerManager = new ProviderManager(authenticationProvider);
+        providerManager.setEraseCredentialsAfterAuthentication(false);
+
+        return providerManager;
+    }
 }
