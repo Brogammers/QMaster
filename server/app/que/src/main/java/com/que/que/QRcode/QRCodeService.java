d7a2e433091fb5253722f65b71cbff3c6966d5d3
FamALouiz <fam@awadlouis.com>
2024-01-22 09:46:17 +0100

    Adding missing imports

--- server/app/que/src/main/java/com/que/que/QRcode/QRCodeService.java
+++ server/app/que/src/main/java/com/que/que/QRcode/QRCodeService.java
@@ -4,18 +4,27 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Service;
 
 import com.google.zxing.BarcodeFormat;
+import com.google.zxing.WriterException;
 import com.google.zxing.client.j2se.MatrixToImageWriter;
 import com.google.zxing.common.BitMatrix;
 import com.google.zxing.qrcode.QRCodeWriter;
 
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
 @Service
 @Configuration
 public class QRCodeService {
-    public void createQRCode(Long queueHolderId, int queueHolderSpecificQueueId, String qrCodeText) {
+    public void createQRCode(Long queueHolderId, int queueHolderSpecificQueueId, String qrCodeText)
+            throws IOException, WriterException {
         QRCodeWriter writer = new QRCodeWriter();
         BitMatrix bitMatrix = writer.encode(qrCodeText, BarcodeFormat.QR_CODE, 300, 150);
         BufferedImage img = MatrixToImageWriter.toBufferedImage(bitMatrix);
-        File output = new File(String.format("resources/static/QRS/%d-%d.png", queueHolderId, queueHolderSpecificQueueId));
+        File output = new File(
+                String.format("resources/static/QRS/%d-%d.png", queueHolderId, queueHolderSpecificQueueId));
         ImageIO.write(img, "png", output);
     }
 }
