fa7e0efa0a064cef90048d94d8d4516ba4077cd3
Hatem Soliman <hatemthedev@gmail.com>
2024-01-23 21:53:15 +0200

    Installed lodash

--- mobile-app/ctx/AuthContext.tsx
+++ mobile-app/ctx/AuthContext.tsx
@@ -1,6 +1,7 @@
 import React, { useContext, useEffect, useState } from 'react';
 import { useStorageState } from '../hooks/useStorageState';
 import { useRouter, useSegments } from 'expo-router';
+import _ from 'lodash';
 
 import { useSelector } from 'react-redux';
 import type { RootState } from '@/app/redux/store';
@@ -51,11 +52,13 @@ export function SessionProvider({ children }: React.PropsWithChildren) {
     }
   };
 
+  const debouncedHandleNavigation = _.debounce(handleNavigation, 500); // Delay by 500ms
+
   useEffect(() => {
     console.log('User:', user); // Log the value of 'user'
     console.log('Root Segment:', rootSegment); // Log the value of 'rootSegment'
 
-    handleNavigation();
+    debouncedHandleNavigation();
   }, [isLoading, session, rootSegment, router]);
 
   useEffect(() => {
