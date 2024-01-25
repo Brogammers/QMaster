a2f4f71bc4238b870a00cd34d0c3f92c227b053a
FamALouiz <fam@awadlouis.com>
2024-01-25 10:07:28 +0100

    Modifying QueueList url

--- server/app/que/src/main/java/com/que/que/QueueList/QueueListController.java
+++ server/app/que/src/main/java/com/que/que/QueueList/QueueListController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 import lombok.AllArgsConstructor;
 
 @RestController
-@RequestMapping(path = "api/v1/queue")
+@RequestMapping(path = "api/v1/queues")
 @AllArgsConstructor
 public class QueueListController {
     private final QueueListService queueListService;
