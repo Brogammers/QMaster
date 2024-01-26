805f01088cfe0d1787eda2b9584cbf28b74fcd77
Mohammed Elazab <mohammedelazab16@gmail.com>
2024-01-26 20:50:53 +0300

    comment

--- mobile-app/components/CurrentQueuesList.tsx
+++ mobile-app/components/CurrentQueuesList.tsx
@@ -16,6 +16,8 @@ export default function CurrentQueuesList() {
   return (
     <View>
       <Text style={{ marginVertical: 10, fontSize: 24, fontWeight: 'bold' }}>Current Queues</Text>
+
+      //Here is proof that the merge works 
       <Carousel
         loop = {false}
         width={carouselWidth}
