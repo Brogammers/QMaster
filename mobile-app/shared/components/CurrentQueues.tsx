ca2724c0cca85d9279a4a9201db5a6fa34224546
Hatem Soliman <hatemthedev@gmail.com>
2024-01-28 00:00:14 +0200

    Replaced blue gradient svg

--- mobile-app/shared/components/CurrentQueues.tsx
+++ mobile-app/shared/components/CurrentQueues.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
-import { Text, TouchableOpacity } from 'react-native';
+import { 
+  Text, 
+  TouchableOpacity, 
+  View 
+} from 'react-native';
 import Image from 'react-native-remote-svg';
-import { View } from 'react-native';  
-import gradient from '@/assets/images/Gradient.png';
 import { FontAwesome5 } from '@expo/vector-icons';
-import arrow from '@/assets/images/arrow-up.svg';
 import { CurrentQueuesProps } from '@/types';
+import gradient from '@/assets/images/Gradient.png';
+import InclineArrow from '@/assets/images/arrow-up.svg';
 
 export default function CurrentQueues(props: CurrentQueuesProps) {
   const { image, name, people, time, isLeave, isJoin, isCurrent, isPopular } = props;
@@ -18,17 +21,29 @@ export default function CurrentQueues(props: CurrentQueuesProps) {
               {isCurrent ? (
                 <FontAwesome5 name="hourglass-start" size={50} color="#D9D9D9" />
               ) : isPopular ? (
-                <Image source={arrow} />
+                <InclineArrow width={50} />
               ) : null}
             </View>
-            <Image source={gradient} className='absolute z-10 self-center w-full h-full' />
-            <Image source={image} className='w-full h-full rounded-[18px]' />
+            <Image 
+              source={gradient} 
+              className='absolute z-10 self-center w-full h-full rounded-l-[18px]' 
+            />
+            <Image 
+              source={image} 
+              className='w-full h-full rounded-l-[18px]' 
+            />
           </View>
           <View className='justify-around w-3/5 pl-2'>
-            <Text className='text-2xl font-black text-white'> {name} </Text>
+            <Text className='text-xl font-black text-white'>
+              {name}
+            </Text>
             <View>
-              <Text className='text-[#D9D9D9] text-base font-medium'> {String(people)} people remaining </Text>
-              <Text className='text-[#D9D9D9] text-xs'> ~{String(time)} min </Text>
+              <Text className='text-[#D9D9D9] text-sm font-medium'>
+                {String(people)} people remaining 
+              </Text>
+              <Text className='text-[#D9D9D9] text-xs'> 
+                ~{String(time)} min
+              </Text>
             </View>
             {isLeave ? (
               <TouchableOpacity className='items-center self-center justify-center w-9/12 mr-2 bg-red-700 rounded-lg h-7'>
