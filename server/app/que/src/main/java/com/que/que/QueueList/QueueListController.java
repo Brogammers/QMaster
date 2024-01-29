526e572b12bce1084f8659b7eaac3d64923cfbbf
FamALouiz <fam@awadlouis.com>
2024-01-29 12:47:30 +0100

    Converting Endpoint to response entity

--- server/app/que/src/main/java/com/que/que/QueueList/QueueListController.java
+++ server/app/que/src/main/java/com/que/que/QueueList/QueueListController.java
@@ -1,7 +1,11 @@
 package com.que.que.QueueList;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -20,13 +24,16 @@ public class QueueListController {
     private final QueueRepository queueRepository;
 
     @GetMapping(path = "/list")
-    public List<Queues> filterSortQueue(@RequestParam("page") int page, @RequestParam("per-page") int perPage,
+    public ResponseEntity<Object> filterSortQueue(@RequestParam("page") int page, @RequestParam("per-page") int perPage,
             @RequestParam("filter") String filterBy, @RequestParam("order") String order) {
+        Map<String, Object> body = new HashMap<>();
+        HttpStatusCode statusCode = HttpStatusCode.valueOf(200);
         if (filterBy != null) {
-            return queueRepository.findByNameContaining(filterBy);
+            body.put("queues", queueRepository.findByNameContaining(filterBy));
         } else {
-            return queueRepository.findAll();
+            body.put("queues", queueRepository.findAll());
         }
+        return new ResponseEntity<Object>(body, statusCode);
     }
 
 }
