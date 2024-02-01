449e289210ec3692f339c195097bcd93ab3f5cac
Hatem Soliman <hatemthedev@gmail.com>
2024-02-01 20:58:40 +0200

    Partially chnages the bgcolor

--- mobile-app/app/(app)/(tabs)/index.tsx
+++ mobile-app/app/(app)/(tabs)/index.tsx
@@ -1,11 +1,12 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
 import {
   StatusBar,
   StyleSheet,
   SafeAreaView,
   Platform,
   View,
-  ScrollView
+  ScrollView,
+  Dimensions
 } from 'react-native';
 import ScanQr from '@/components/ScanQR';
 import SearchBar from '@/components/SearchBar';
@@ -16,17 +17,36 @@ import CurrentQueuesList from '@/components/CurrentQueuesList';
 
 
 export default function Index() {
+  const [bgColor, setBgColor] = useState('#17222D');
+  const scanQrRef = useRef<View>(null);
+
+  const handleScroll = () => {
+    scanQrRef.current?.measure((fx, fy, width, height, px, py) => {
+      if (py < 0) {
+        setBgColor('#D9D9D9');
+      } else {
+        setBgColor('#17222D');
+      }
+    });
+  };
 
   return (
-    <SafeAreaView style={styles.container}>
+    <SafeAreaView style={[styles.container, { backgroundColor: bgColor }]}>
       <StatusBar
         translucent
         backgroundColor='#17222D'
         barStyle='light-content'
       />
-      <ScrollView className='w-screen' showsVerticalScrollIndicator={false}>
+      <ScrollView
+        className='w-screen'
+        showsVerticalScrollIndicator={false}
+        scrollEventThrottle={16}
+        onScroll={handleScroll}
+      >
         <View className='bg-[#D9D9D9]'>
-          <ScanQr />
+          <View ref={scanQrRef}>
+            <ScanQr />
+          </View>
           <View className='w-[85%] self-center'>
             <SearchBar />
             <CurrentQueuesList />
@@ -43,7 +63,7 @@ export default function Index() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#17222D',
+    // backgroundColor: '#17222D',
     paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight ? StatusBar.currentHeight - 1 : 0 : 0,
   },
 });
