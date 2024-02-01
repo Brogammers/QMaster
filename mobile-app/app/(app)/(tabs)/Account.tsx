dd7fb4ca71c90ab215b09c300bcecfb62823a2fd
Hatem Soliman <hatemthedev@gmail.com>
2024-02-01 20:47:17 +0200

    Updated status bar lighting for History and Account

--- mobile-app/app/(app)/(tabs)/Account.tsx
+++ mobile-app/app/(app)/(tabs)/Account.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Text, View, StatusBar, StyleSheet, Platform } from "react-native";
+import { Text, View, StatusBar, Platform, StyleSheet } from "react-native";
 import AccountPageItems from '@/components/AccountPageItems'
 import { SafeAreaView } from "react-native-safe-area-context";
 import AccountPageProfile from "@/components/AccountPageProfile";
@@ -7,22 +7,20 @@ import { ScrollView } from "react-native-gesture-handler";
 
 export default function Account() {
   return (
-    <SafeAreaView style={styles.container} className="items-center self-center flex-1">
+    <SafeAreaView className="items-center self-center flex-1">
       <StatusBar
         translucent
         backgroundColor='#17222D'
-        barStyle='light-content'
+        barStyle='dark-content'
       />
-      <View className="bg-[#D9D9D9] w-full h-full">
-        <AccountPageProfile />
-        <View className="w-[85%] flex flex-col items-center justify-center">
-          <ScrollView>
-            <Text className="w-full pb-6 text-3xl font-extrabold text-left border-b text-ocean-blue-2 border-[#ADADAD]">
-              Hi John!
-            </Text>
-            <AccountPageItems />
-          </ScrollView>
-        </View>
+      <AccountPageProfile />
+      <View className="w-[85%]">
+        <ScrollView>
+          <Text className="w-full pb-6 text-3xl font-extrabold text-left border-b text-ocean-blue-2 border-[#ADADAD]">
+            Hi John!
+          </Text>
+          <AccountPageItems />
+        </ScrollView>
       </View>
     </SafeAreaView>
   )
@@ -30,9 +28,8 @@ export default function Account() {
 
 const styles = StyleSheet.create({
   container: {
-    width: '100%',
     flex: 1,
-    backgroundColor: '#17222D',
+    backgroundColor: '#D9D9D9',
     paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight ? StatusBar.currentHeight - 1 : 0 : 0,
   },
 });
--- mobile-app/app/(app)/(tabs)/History.tsx
+++ mobile-app/app/(app)/(tabs)/History.tsx
@@ -1,29 +1,23 @@
 import React from "react";
 import { Platform, StatusBar, StyleSheet, Text, View } from "react-native";
-import { ScrollView } from "react-native-gesture-handler";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function History() {
   return (
     <SafeAreaView style={styles.container}>
-    <StatusBar
-      translucent
-      backgroundColor='#17222D'
-      barStyle='light-content'
-    />
-    <ScrollView className='w-screen' showsVerticalScrollIndicator={false}>
-      <View className='bg-[#D9D9D9]'>
-        <Text>History</Text>
-      </View>
-    </ScrollView>
-  </SafeAreaView>
-  )
+      <StatusBar
+        translucent
+        backgroundColor='#17222D'
+        barStyle='dark-content'
+      />
+    </SafeAreaView>
+  );
 }
-
+  
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#17222D',
+    backgroundColor: '#D9D9D9',
     paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight ? StatusBar.currentHeight - 1 : 0 : 0,
   },
 });
