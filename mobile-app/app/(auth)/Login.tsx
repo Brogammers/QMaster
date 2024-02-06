fba992c92337b70bf4099377e98bf92545a84903
Hatem Soliman <hatemthedev@gmail.com>
2024-02-06 23:52:43 +0200

    Added error exceptions and the dungeons are filled with dragons

--- mobile-app/app/(auth)/Login.tsx
+++ mobile-app/app/(auth)/Login.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
 import {
   StyleSheet,
   ImageBackground,
@@ -8,32 +8,33 @@ import {
   TextInput,
   Alert,
   StatusBar,
-} from 'react-native';
-import { Link } from 'expo-router';
-import { Formik } from 'formik';
-import * as Yup from 'yup';
-import TextButton from '@/shared/components/TextButton';
-import Return from '@/shared/components/Return';
-import background from '@/assets/images/background.png';
-import LoginImg from '@/assets/images/login.png';
-import axios, { AxiosError } from 'axios';
-import { useAuth } from '@/ctx/AuthContext';
-import { API_BASE_URL_LOGIN, API_BASE_URL_LOGIN_ANDROID } from '@env';
+} from "react-native";
+import { Link } from "expo-router";
+import { Formik } from "formik";
+import * as Yup from "yup";
+import TextButton from "@/shared/components/TextButton";
+import Return from "@/shared/components/Return";
+import background from "@/assets/images/background.png";
+import LoginImg from "@/assets/images/login.png";
+import axios, { AxiosError } from "axios";
+import { useAuth } from "@/ctx/AuthContext";
+import { API_BASE_URL_LOGIN, API_BASE_URL_LOGIN_ANDROID } from "@env";
 
-import { useDispatch } from 'react-redux';
-import { setEmail } from '../redux/authSlice'; // replace with the actual path
-
+import { useDispatch } from "react-redux";
+import { setEmail } from "../redux/authSlice";
+import { isEmpty } from "lodash";
 
 const LoginSchema = Yup.object().shape({
-  email: Yup.string().email('Invalid email').required("Email required"),
+  email: Yup.string().email("Invalid email").required("Email required"),
   password: Yup.string().required("Password required"),
 });
 
 export default function Login() {
   const dispatch = useDispatch();
   const auth = useAuth();
+  const [errorMessage, setErrorMessage] = useState("");
 
-  const handleLogin = async (values: any) => {
+  const handleLogin = async (values: any, formikHelpers: { setSubmitting: (isSubmitting: boolean) => void, setErrors: (errors: { [field: string]: string }) => void }) => {
     console.log("Logging in...", values);
 
     try {
@@ -42,14 +43,17 @@ export default function Login() {
       // Android Emulator
       // const response = await axios.post('http://10.0.2.2:8080/api/v1/login', values);
 
-
       if (response.status === 200 || response.status === 201) {
-        console.log('Login successful', values);
+        console.log("Login successful", values);
         if (auth && auth.signIn) {
           console.log("This is the type of response: " + typeof response.data);
-          console.log("This is the email of the user: ", response.data.email, " and type: ", typeof response.data.email)
-          // Update the session state and wait for the update to complete
-          await new Promise<void>(resolve => {
+          console.log(
+            "This is the email of the user: ",
+            response.data.email,
+            " and type: ",
+            typeof response.data.email
+          );
+          await new Promise<void>((resolve) => {
             dispatch(setEmail(response.data.email));
             resolve();
           });
@@ -57,56 +61,63 @@ export default function Login() {
           auth.signIn();
         }
       } else {
-        console.error('Login failed', response.data);
-        Alert.alert('Signup Failed', 'Please check your input and try again.');
+        console.error("Login failed", response.data);
+        Alert.alert("Signup Failed", "Please check your input and try again.");
       }
     } catch (error) {
-      console.error('Login error:', error);
-      Alert.alert('Error', 'An unexpected error occurred. Please try again later.');
-
       if (axios.isAxiosError(error)) {
         const axiosError = error as AxiosError;
 
         if (axiosError.response) {
-          console.error('Axios error status:', axiosError.response.status);
-          console.error('Axios error data:', axiosError.response.data);
+          if (typeof axiosError.response.data === "string") {
+            setErrorMessage(axiosError.response.data);
+          } 
+          // else if (typeof axiosError.response.data === "object") {
+          //   setErrors(axiosError.response.data);
+          // }
         } else if (axiosError.request) {
-          console.error('Axios error request:', axiosError.request);
+          setErrorMessage(axiosError.request);
         } else {
-          console.error('Axios error message:', axiosError.message);
+          setErrorMessage(axiosError.message);
         }
+      } else if (error instanceof Error) {
+        setErrorMessage(error.message);
       } else {
-        // Handle non-Axios errors
-        console.error('Non-Axios error:', error);
+        setErrorMessage("An error occurred");
       }
     }
   };
 
+  useEffect(() => {
+    if (!isEmpty(errorMessage)) {
+      Alert.alert("Error", errorMessage);
+    }
+  }, [errorMessage]);
+
   return (
     <ImageBackground source={background} style={styles.container}>
-      <Link href='/Onboarding' style={styles.returnButton}>
-        <Return size={36} color='white' />
+      <Link href="/Onboarding" style={styles.returnButton}>
+        <Return size={36} color="white" />
       </Link>
       <StatusBar
         translucent
-        backgroundColor='rgba(000, 000, 000, 0.5)'
-        barStyle='light-content'
+        backgroundColor="rgba(000, 000, 000, 0.5)"
+        barStyle="light-content"
       />
       <View style={styles.row}>
-        <Text
-          style={styles.title}
-          className='mb-10 text-2xl text-white mt-14'
-        >
+        <Text style={styles.title} className="mb-10 text-2xl text-white mt-14">
           Welcome Back!
         </Text>
-        <Image source={LoginImg} className='mt-6 mb-12' />
+        <Image source={LoginImg} className="mt-6 mb-12" />
         <Formik
           initialValues={{
-            email: '',
-            password: '',
+            email: "",
+            password: "",
           }}
           validationSchema={LoginSchema}
-          onSubmit={handleLogin}
+          onSubmit={(values, formikHelpers) =>
+            handleLogin(values, formikHelpers)
+          }
         >
           {({
             handleChange,
@@ -115,51 +126,64 @@ export default function Login() {
             values,
             touched,
             errors,
-            isValid
+            isValid,
           }) => (
-            <View className='flex items-center justify-center w-full gap-4'>
+            <View className="flex items-center justify-center w-full gap-4">
               <TextInput
                 style={styles.input}
-                placeholder='Enter your email'
-                placeholderTextColor={'#515151'}
-                onChangeText={handleChange('email')}
-                keyboardType='email-address'
+                placeholder="Enter your email"
+                placeholderTextColor={"#515151"}
+                onChangeText={handleChange("email")}
+                keyboardType="email-address"
                 value={values.email}
-                autoCapitalize='none'
+                autoCapitalize="none"
               />
-              {(errors.email && touched.email) &&
-                <Text style={{ fontSize: 12, color: 'red', textAlign: 'center' }}>{errors.email}</Text>
-              }
+              {errors.email && touched.email && (
+                <Text
+                  style={{ fontSize: 12, color: "red", textAlign: "center" }}
+                >
+                  {errors.email}
+                </Text>
+              )}
               <TextInput
                 style={styles.input}
-                placeholder='Enter your password'
-                placeholderTextColor={'#515151'}
-                onChangeText={handleChange('password')}
-                onBlur={handleBlur('password')}
+                placeholder="Enter your password"
+                placeholderTextColor={"#515151"}
+                onChangeText={handleChange("password")}
+                onBlur={handleBlur("password")}
                 secureTextEntry
                 value={values.password}
               />
-              {(errors.password && touched.password) &&
-                <Text style={{ fontSize: 12, color: 'red', textAlign: 'center' }}>{errors.password}</Text>
-              }
-              <Text
-                className='mt-2 text-sm underline text-baby-blue'
-              >
+              {errors.password && touched.password && (
+                <Text
+                  style={{ fontSize: 12, color: "red", textAlign: "center" }}
+                >
+                  {errors.password}
+                </Text>
+              )}
+              {errorMessage !== "" && (
+                <Text
+                  style={{ fontSize: 12, color: "red", textAlign: "center" }}
+                >
+                  {errorMessage.toString()}
+                </Text>
+              )}
+              <Text className="mt-2 text-sm underline text-baby-blue">
                 Forgot password?
               </Text>
-              <View className='mt-8'>
+              <View className="mt-8">
                 <TextButton
-                  text={'Log In'}
-                  buttonColor={!isValid ? '#C5C5C5' : '#1DCDFE'}
-                  textColor={'white'}
+                  text={"Log In"}
+                  buttonColor={!isValid ? "#C5C5C5" : "#1DCDFE"}
+                  textColor={"white"}
                   disabled={!isValid}
                   onPress={handleSubmit}
                 />
                 <TextButton
-                  text={'Continue with Google'}
-                  icon={'google'}
-                  buttonColor={'white'}
-                  textColor={'#17222D'}
+                  text={"Continue with Google"}
+                  icon={"google"}
+                  buttonColor={"white"}
+                  textColor={"#17222D"}
                 />
               </View>
             </View>
@@ -167,86 +191,84 @@ export default function Login() {
         </Formik>
       </View>
     </ImageBackground>
-  )
+  );
 }
-
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: '#17222D',
-    color: '#FFF',
+    backgroundColor: "#17222D",
+    color: "#FFF",
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center",
   },
   row: {
-    width: '80%',
-    display: 'flex',
+    width: "80%",
+    display: "flex",
     flex: 1,
-    flexDirection: 'column',
-    justifyContent: 'center',
-    alignItems: 'center',
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
   },
   returnButton: {
-    position: 'absolute',
+    position: "absolute",
     top: 60,
     left: 18,
   },
   title: {
-    fontFamily: 'InterBold',
+    fontFamily: "InterBold",
     fontSize: 28,
-    color: '#FFF',
+    color: "#FFF",
     marginBottom: 16,
   },
   baseText: {
     fontSize: 16,
-    color: '#FFF',
+    color: "#FFF",
     marginBottom: 40,
   },
   input: {
-    backgroundColor: '#DFDFDF',
-    color: '#515151',
+    backgroundColor: "#DFDFDF",
+    color: "#515151",
     fontSize: 16,
-    fontFamily: 'InterBold',
+    fontFamily: "InterBold",
     borderRadius: 42,
     height: 56,
     marginBottom: 5,
     paddingVertical: 4,
     paddingHorizontal: 24,
-    width: '100%'
+    width: "100%",
   },
   button: {
     borderRadius: 10,
-    overflow: 'hidden',
+    overflow: "hidden",
   },
   signUpButton: {
-    backgroundColor: '#1DCDFE',
+    backgroundColor: "#1DCDFE",
     marginTop: 10,
     paddingVertical: 16,
     paddingHorizontal: 12,
-    flexDirection: 'row',
-    justifyContent: 'center',
-    alignItems: 'center',
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
   },
   signUpButtonText: {
     fontSize: 16,
-    color: '#FFF',
-    textAlign: 'center',
+    color: "#FFF",
+    textAlign: "center",
   },
   googleButton: {
-    backgroundColor: '#FFF',
-    color: '#17222D',
+    backgroundColor: "#FFF",
+    color: "#17222D",
     marginTop: 10,
     paddingVertical: 16,
     paddingHorizontal: 12,
-    flexDirection: 'row',
-    justifyContent: 'center',
-    alignItems: 'center',
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
   },
   googleButtonText: {
     fontSize: 16,
-    color: '#17222D',
-    textAlign: 'center',
+    color: "#17222D",
+    textAlign: "center",
   },
 });
-
