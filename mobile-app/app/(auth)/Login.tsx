fe8b18a93bf186a4789c2aea84d6bdfcdcf8790f
Mohammed Elazab <mohammedelazab16@gmail.com>
2024-02-09 16:27:42 +0200

    Rate App

--- mobile-app/app/(auth)/Login.tsx
+++ mobile-app/app/(auth)/Login.tsx
@@ -43,9 +43,9 @@ export default function Login() {
 
     try {
       // IOS Simulator
-      const response = await axios.post(`${API_BASE_URL_LOGIN}`, values);
+      // const response = await axios.post(`${API_BASE_URL_LOGIN}`, values);
       // Android Emulator
-      // const response = await axios.post('http://10.0.2.2:8080/api/v1/login', values);
+       const response = await axios.post('http://10.0.2.2:8080/api/v1/login', values);
 
       if (response.status === 200 || response.status === 201) {
         console.log("Login successful", values);
--- mobile-app/components/AccountPageItems.tsx
+++ mobile-app/components/AccountPageItems.tsx
@@ -1,18 +1,29 @@
 import { useAuth } from "@/ctx/AuthContext";
 import React from "react";
-import { View } from "react-native";
+import { View, Linking, Platform } from "react-native";
 import SearchItem from "@/shared/components/SearchItem";
 
 export default function AccountPageItems() {
   const auth = useAuth();
 
+  const handleRateApp = () => {
+    const storeUrl = Platform.select({
+      ios: 'https://apps.apple.com/us/app/your-app-id',
+      android: 'market://details?id=com.yourapp.package',
+    })|| '';
+
+    Linking.openURL(storeUrl).catch((err) =>
+      console.error('An error occurred', err)
+    );
+  };
+
   return (
     <View className="items-center justify-center w-full">
-      <SearchItem title={'Language and Region'} icon={'globe'} isAccount onPress={() => console.log('Language and Region')} />
-      <SearchItem title={'Previous Queues'} icon={'clock-rotate-left'} isAccount onPress={() => console.log('Previous Queues')} />
+      <SearchItem title={'Language'} icon={'globe'} isAccount onPress={() => console.log('Language and Region')} />
+      {/* <SearchItem title={'Previous Queues'} icon={'clock-rotate-left'} isAccount onPress={() => console.log('Previous Queues')} /> */}
       <SearchItem title={'Notifications'} icon={'bell'} isAccount onPress={() => console.log('Notifications')} />
       <SearchItem title={'About'} icon={'circle-info'} isAccount onPress={() => console.log('About')} />
-      <SearchItem title={'Rate App'} icon={'star'} isAccount onPress={() => console.log('Rate App')} />
+      <SearchItem title={'Rate App'} icon={'star'} isAccount onPress={handleRateApp} />
       <SearchItem title={'Log Out'} icon={'arrow-right-from-bracket'} isAccount onPress={auth?.signOut} />
     </View>
   )
