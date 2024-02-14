7f38896b10a5ade7bd9d8cc0ebb9c309eac336da
Hatem Soliman <hatemthedev@gmail.com>
2024-02-14 14:24:48 +0200

    Added the router to replace current route to missing

--- mobile-app/app/(app)/(tabs)/index.tsx
+++ mobile-app/app/(app)/(tabs)/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useEffect, useRef, useState } from "react";
 import {
   StatusBar,
   StyleSheet,
@@ -6,31 +6,34 @@ import {
   Platform,
   View,
   ScrollView,
-  Pressable,
   Text,
-} from 'react-native';
-import ScanQr from '@/components/ScanQR';
-import SearchBar from '@/components/SearchBar';
-import CategoriesList from '@/components/CategoriesList';
-import RecentQueues from '@/components/RecentQueues';
-import FrequentlyAsked from '@/components/FrequentlyAsked';
-import CurrentQueuesList from '@/components/CurrentQueuesList';
-import { TouchableOpacity } from 'react-native-gesture-handler';
-import { Link } from 'expo-router';
-
+} from "react-native";
+import ScanQr from "@/components/ScanQR";
+import SearchBar from "@/components/SearchBar";
+import CategoriesList from "@/components/CategoriesList";
+import RecentQueues from "@/components/RecentQueues";
+import FrequentlyAsked from "@/components/FrequentlyAsked";
+import CurrentQueuesList from "@/components/CurrentQueuesList";
+import { TouchableOpacity } from "react-native-gesture-handler";
+import { useRouter } from "expo-router";
 
 export default function Index() {
-  const [bgColor, setBgColor] = useState('#17222D');
+  const [bgColor, setBgColor] = useState("#17222D");
   const [scrollY, setScrollY] = useState(0);
   const scanQrRef = useRef<View>(null);
   const [scanQrHeight, setScanQrHeight] = useState(0);
+  const router = useRouter();
 
-  const handleScroll = (event: { 
-    nativeEvent: { 
-      contentOffset: { 
-        y: any; 
-      }; 
-    }; 
+  const handleMissing = () => {
+    router.replace("/Missing");
+  };
+
+  const handleScroll = (event: {
+    nativeEvent: {
+      contentOffset: {
+        y: any;
+      };
+    };
   }) => {
     const currentScrollY = event.nativeEvent.contentOffset.y;
     setScrollY(currentScrollY);
@@ -38,21 +41,23 @@ export default function Index() {
 
   useEffect(() => {
     if (scrollY > scanQrHeight) {
-      setBgColor('#D9D9D9');
-      if (Platform.OS === 'android') {
-        StatusBar.setBackgroundColor('#D9D9D9', true);
+      setBgColor("#D9D9D9");
+      if (Platform.OS === "android") {
+        StatusBar.setBackgroundColor("#D9D9D9", true);
       }
-      StatusBar.setBarStyle('dark-content');
+      StatusBar.setBarStyle("dark-content");
     } else {
-      setBgColor('#17222D');
-      if (Platform.OS === 'android') {
-        StatusBar.setBackgroundColor('#17222D', true);
+      setBgColor("#17222D");
+      if (Platform.OS === "android") {
+        StatusBar.setBackgroundColor("#17222D", true);
       }
-      StatusBar.setBarStyle('light-content');
+      StatusBar.setBarStyle("light-content");
     }
-  }, [scrollY, scanQrHeight]);  
+  }, [scrollY, scanQrHeight]);
 
-  const handleLayout = (event: { nativeEvent: { layout: { height: React.SetStateAction<number>; }; }; }) => {
+  const handleLayout = (event: {
+    nativeEvent: { layout: { height: React.SetStateAction<number> } };
+  }) => {
     setScanQrHeight(event.nativeEvent.layout.height);
   };
 
@@ -60,31 +65,30 @@ export default function Index() {
     <SafeAreaView style={[styles.container, { backgroundColor: bgColor }]}>
       <StatusBar
         translucent
-        backgroundColor='#17222D'
-        barStyle= {scrollY > scanQrHeight ? 'dark-content' : 'light-content'}
+        backgroundColor="#17222D"
+        barStyle={scrollY > scanQrHeight ? "dark-content" : "light-content"}
       />
       <ScrollView
-        className='w-screen'
+        className="w-screen"
         showsVerticalScrollIndicator={false}
         scrollEventThrottle={16}
         onScroll={handleScroll}
       >
-        <View className='bg-off-white'>
+        <View className="bg-off-white">
           <View ref={scanQrRef} onLayout={handleLayout}>
             <ScanQr />
           </View>
-          <View className='w-10/12 self-center'>
+          <View className="w-10/12 self-center">
             <SearchBar />
             <CurrentQueuesList />
             <CategoriesList />
             <RecentQueues />
             <FrequentlyAsked />
-            <TouchableOpacity className='bg-baby-blue flex items-center justify-center py-4 rounded-sxl'>
-              <Link href="/Missing">
-                <Text className='text-white font-bold text-lg'>
-                  Missing
-                </Text>
-              </Link>
+            <TouchableOpacity
+              className="bg-baby-blue flex items-center justify-center py-4 rounded-sxl"
+              onPress={handleMissing}
+            >
+              <Text className="text-white font-bold text-lg">Missing</Text>
             </TouchableOpacity>
           </View>
         </View>
@@ -96,6 +100,11 @@ export default function Index() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight ? StatusBar.currentHeight - 1 : 0 : 0,
+    paddingTop:
+      Platform.OS === "android"
+        ? StatusBar.currentHeight
+          ? StatusBar.currentHeight - 1
+          : 0
+        : 0,
   },
 });
