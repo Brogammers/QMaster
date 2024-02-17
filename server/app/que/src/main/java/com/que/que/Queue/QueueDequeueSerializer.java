a70b46481b4af9ef572d01bb3f05c7f71c8cdc15
Fam Awad <fam@awadlouis.com>
2024-02-18 00:42:55 +0200

    Adding queue dequeue serializer

---
+++ server/app/que/src/main/java/com/que/que/Queue/QueueDequeueSerializer.java
@@ -0,0 +1,32 @@
+package com.que.que.Queue;
+
+import java.io.IOException;
+
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import com.fasterxml.jackson.databind.ser.std.StdSerializer;
+
+public class QueueDequeueSerializer extends StdSerializer<QueueDequeue> {
+
+  public QueueDequeueSerializer() {
+    this(null);
+  }
+
+  public QueueDequeueSerializer(Class<QueueDequeue> t) {
+    super(t);
+  }
+
+  @Override
+  public void serialize(
+      QueueDequeue value, JsonGenerator jgen, SerializerProvider provider)
+      throws IOException, JsonProcessingException {
+
+    jgen.writeStartObject();
+    jgen.writeNumberField("id", value.getId());
+    jgen.writeStringField("name", value.getQueue().getName());
+    jgen.writeObjectField("time", value.getActionDate());
+    jgen.writeStringField("notification", "You queued successfully!");
+    jgen.writeEndObject();
+  }
+}
