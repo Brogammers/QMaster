e34a881fd5677a22a6f82192f0badb4dbc145be7
Fam Awad <fam@awadlouis.com>
2024-02-18 00:06:21 +0200

    Adding queue dequeue history as well

--- server/app/que/src/main/java/com/que/que/History/HistoryService.java
+++ server/app/que/src/main/java/com/que/que/History/HistoryService.java
@@ -1,9 +1,12 @@
 package com.que.que.History;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.stereotype.Service;
 
+import com.que.que.Queue.QueueDequeue;
+import com.que.que.Queue.QueueDequeueRepository;
 import com.que.que.Queue.QueueEnqueue;
 import com.que.que.Queue.QueueEnqueueRepository;
 import com.que.que.User.AppUser;
@@ -15,12 +18,17 @@ import lombok.AllArgsConstructor;
 @AllArgsConstructor
 public class HistoryService {
     private final QueueEnqueueRepository queueEnqueueRepository;
+    private final QueueDequeueRepository queueDequeueRepository;
     private final AppUserRepository appUserRepository;
 
-    public List<QueueEnqueue> getUserHistory(Long id) {
+    public List<Object> getUserHistory(Long id) {
         AppUser appUser = appUserRepository.findById(id)
                 .orElseThrow(() -> new IllegalStateException("Could not find user with such id"));
-        List<QueueEnqueue> history = queueEnqueueRepository.findByAppUser(appUser);
-        return history;
+        List<Object> result = new ArrayList<>();
+        List<QueueEnqueue> historyQueue = queueEnqueueRepository.findByAppUser(appUser);
+        List<QueueDequeue> historyDequeue = queueDequeueRepository.findByAppUser(appUser);
+        result.addAll(historyDequeue);
+        result.addAll(historyQueue);
+        return result;
     }
 }
