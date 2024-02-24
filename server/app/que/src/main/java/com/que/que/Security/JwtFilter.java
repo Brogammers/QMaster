9624d8ccc593f225409aab17ba42bae86940838c
Fam Awad <fam@awadlouis.com>
2024-02-24 21:26:13 +0200

    Adding JWT token util file and filteR

---
+++ server/app/que/src/main/java/com/que/que/Security/JwtFilter.java
@@ -0,0 +1,55 @@
+package com.que.que.Security;
+
+import java.io.IOException;
+import java.util.List;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import com.que.que.User.AppUser;
+import com.que.que.User.AppUserRepository;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.AllArgsConstructor;
+import org.springframework.util.StringUtils;
+
+@Component
+@AllArgsConstructor
+public class JwtFilter extends OncePerRequestFilter {
+
+  private final AppUserRepository appUserRepository;
+  private final JwtUtil jwtUtil;
+
+  @Override
+  protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+      throws ServletException, IOException {
+    // Get authorization header and validate
+    String header = request.getHeader(HttpHeaders.AUTHORIZATION);
+    if (!header.startsWith("Bearer ") || !StringUtils.hasText(header)) {
+      filterChain.doFilter(request, response);
+      return;
+    }
+
+    String token = header.split(" ")[1].trim();
+    AppUser user = appUserRepository.findByEmail(jwtUtil.getUsername(token)).orElse(null);
+    if (!jwtUtil.validateToken(token, user)) {
+      filterChain.doFilter(request, response);
+      return;
+    }
+
+    UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(user, null,
+        user == null ? List.of() : user.getAuthorities());
+
+    authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+
+    SecurityContextHolder.getContext().setAuthentication(authentication);
+    filterChain.doFilter(request, response);
+  }
+}
