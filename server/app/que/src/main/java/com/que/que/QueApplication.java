efcb830f45b7c8d4698483e16a37f4879d0d26b4
Fam Awad <fam@awadlouis.com>
2024-03-15 14:47:13 +0100

    Adding server servlet initializer

--- server/app/que/src/main/java/com/que/que/QueApplication.java
+++ server/app/que/src/main/java/com/que/que/QueApplication.java
@@ -2,10 +2,16 @@ package com.que.que;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class QueApplication {
+public class QueApplication extends SpringBootServletInitializer{
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application){
+		return application.sources(QueApplication.class);
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(QueApplication.class, args);
 	}
