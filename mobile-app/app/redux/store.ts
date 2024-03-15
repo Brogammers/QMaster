b03bc1b31b10bb8595601be49ac8dc52edf9bacf
Hatem Soliman <hatemthedev@gmail.com>
2024-03-15 10:40:42 +0200

    Removed redundant redux slices

--- mobile-app/app/redux/store.ts
+++ mobile-app/app/redux/store.ts
@@ -1,13 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import authSlice from './authSlice';
 import userSlice from './userSlice';
-import navigationSlice from './navigationSlice';
 
 export const store = configureStore({
   reducer: {
     emailSetter: authSlice,
     username: userSlice,
-    navigationSetter: navigationSlice,
   },
 })
 
