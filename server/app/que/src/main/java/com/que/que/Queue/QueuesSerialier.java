e5269ebaab80b39feec192a554f71cfc8889470b
Fam Awad <fam@awadlouis.com>
2024-03-15 09:40:16 +0100

    Adding queue serialier

---
+++ server/app/que/src/main/java/com/que/que/Queue/QueuesSerialier.java
@@ -0,0 +1,26 @@
+package com.que.que.Queue;
+
+import java.io.IOException;
+
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import com.fasterxml.jackson.databind.ser.std.StdSerializer;
+
+public class QueuesSerialier extends StdSerializer<Queues>{
+
+    public QueuesSerialier(){
+      this(null);
+    }
+
+    public QueuesSerialier(Class<Queues> t){
+      super(t);
+    }
+
+    @Override
+    public void serialize(Queues value, JsonGenerator jgen, SerializerProvider provider) throws IOException {
+      jgen.writeStartObject();
+      jgen.writeStringField("name", value.getName());
+      jgen.writeNumberField("people", value.getPeopleInQueue());
+    }
+    
+}
