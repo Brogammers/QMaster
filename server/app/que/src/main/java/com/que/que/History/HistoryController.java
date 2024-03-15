693a6c7d5a192816c201751df71a4f70b79f75d3
Hatem Soliman <hatemthedev@gmail.com>
2024-03-15 11:10:25 +0200

    Resolved merge conflicts

--- server/app/que/src/main/java/com/que/que/History/HistoryController.java
+++ server/app/que/src/main/java/com/que/que/History/HistoryController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.que.que.Queue.QueueEnqueue;
 
 import lombok.AllArgsConstructor;
 
