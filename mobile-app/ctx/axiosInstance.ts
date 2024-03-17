ffde3d1b72f28914adb11ea452c0ba1cc7ccd75f
Hatem Soliman <hatemthedev@gmail.com>
2024-03-17 19:46:14 +0200

    Added type of

--- mobile-app/ctx/axiosInstance.ts
+++ mobile-app/ctx/axiosInstance.ts
@@ -7,6 +7,7 @@ const BASE_URL = "http://localhost:8080/api/v1";
 
 async function getToken() {
   const token = await AsyncStorage.getItem("token");
+  console.log("Type of token: ", typeof token);
   return token?.toString();
 }
 
