e2f2e3a0130518f5d2c2d8a2ba1702ff45c84251
Fam Awad <fam@awadlouis.com>
2024-03-17 18:41:51 +0100

    Returning string not object

--- mobile-app/ctx/axiosInstance.ts
+++ mobile-app/ctx/axiosInstance.ts
@@ -7,7 +7,7 @@ const BASE_URL = "http://localhost:8080/api/v1";
 
 async function getToken() {
   const token = await AsyncStorage.getItem("token");
-  return token;
+  return token?.toString();
 }
 
 const axiosInstance = axios.create({
