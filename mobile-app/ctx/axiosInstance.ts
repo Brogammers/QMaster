38b362c6ac23936d7776ab92680412847501d0aa
Fam Awad <fam@awadlouis.com>
2024-03-17 18:40:19 +0100

    Modifying json string

--- mobile-app/ctx/axiosInstance.ts
+++ mobile-app/ctx/axiosInstance.ts
@@ -1,25 +1,25 @@
-import axios from 'axios';
-import AsyncStorage from '@react-native-async-storage/async-storage';
-import { Alert } from 'react-native';
-
+import axios from "axios";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { Alert } from "react-native";
 
 // Define your base URL (adjust this to your actual backend URL)
-const BASE_URL = 'http://localhost:8080/api/v1';
+const BASE_URL = "http://localhost:8080/api/v1";
 
 async function getToken() {
-  const token = await AsyncStorage.getItem('token');
+  const token = await AsyncStorage.getItem("token");
   return token;
 }
 
 const axiosInstance = axios.create({
-  // baseURL: BASE_URL, 
+  // baseURL: BASE_URL,
   timeout: 5000,
   headers: {
     Authorization: `Bearer ${getToken()}`,
-    'Content-Type': 'application/json'
-  }
+    "Content-Type": "application/json",
+  },
 });
 
+console.log(`Bearer ${getToken()}`);
 // const axiosInstance = axios.create();
 
 // axiosInstance.interceptors.request.use(
@@ -39,8 +39,8 @@ const axiosInstance = axios.create({
 // );
 
 axiosInstance.interceptors.response.use(
-  response => response,
-  error => {
+  (response) => response,
+  (error) => {
     // Handle 401 Unauthorized errors here
     if (error.response && error.response.status === 401) {
       // Handle unauthorized access (e.g., logout user, redirect to login page)
