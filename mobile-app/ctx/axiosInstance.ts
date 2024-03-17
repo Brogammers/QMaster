457d82e47083040bd6db930a0b254f17c8d8af46
Fam Awad <fam@awadlouis.com>
2024-03-17 19:11:57 +0100

    Adding console.log() statement for debugging '

--- mobile-app/ctx/axiosInstance.ts
+++ mobile-app/ctx/axiosInstance.ts
@@ -7,6 +7,7 @@ const BASE_URL = "http://localhost:8080/api/v1";
 
 async function getToken() {
   const token = await AsyncStorage.getItem("token");
+  console.log(token);
   return token?.toString();
 }
 
