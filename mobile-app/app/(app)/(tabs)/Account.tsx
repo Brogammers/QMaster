70481a9dfc6c9726055990af10f2866be9b3e145
Hatem Soliman <hatemthedev@gmail.com>
2024-03-20 19:32:50 +0200

    Added skeleton loading to Account

--- mobile-app/app/(app)/(tabs)/Account.tsx
+++ mobile-app/app/(app)/(tabs)/Account.tsx
@@ -1,18 +1,43 @@
-import React from "react";
-import { Text, View, StatusBar } from "react-native";
+import React, { useEffect, useState } from "react";
+import { Text, View, StatusBar, useWindowDimensions } from "react-native";
 import AccountPageItems from "@/components/AccountPageItems";
 import { SafeAreaView } from "react-native-safe-area-context";
 import AccountPageProfile from "@/components/AccountPageProfile";
 import { ScrollView } from "react-native-gesture-handler";
 import { useSelector } from "react-redux";
 import { RootState } from "@/app/redux/store";
+import { Skeleton } from "moti/skeleton";
 
 export default function Account() {
+  const [isLoading, setIsLoading] = useState(true);
+  const colorMode: 'light' | 'dark' = 'light';
+  const windowWidth = useWindowDimensions().width;
+
   const firstName = useSelector((state: RootState) => state.username.username)?.split(' ')[0];
+
+  useEffect(() => {
+    const timeoutId = setTimeout(() => {
+      setIsLoading(false);
+    }, 2000);
+
+    if (firstName) {
+      setIsLoading(false);
+      clearTimeout(timeoutId); 
+    }
+
+    return () => clearTimeout(timeoutId);
+  }, [firstName]);
 
   return (
     <SafeAreaView className="items-center self-center flex-1 w-screen bg-off-white">
       <StatusBar translucent barStyle="dark-content" />
+      {/* <Skeleton
+        colorMode={colorMode}
+        width={windowWidth * (11/12)}
+        height={100}
+      >
+        {isLoading ? null : <AccountPageProfile />}
+      </Skeleton> */}
       <AccountPageProfile />
       <View className="w-10/12">
         <ScrollView>
--- mobile-app/components/AccountPageProfile.tsx
+++ mobile-app/components/AccountPageProfile.tsx
@@ -1,30 +1,57 @@
-import React from "react";
-import { View, Text, Image } from "react-native";
+import React, { useEffect, useState } from "react";
+import { View, Text, Image, useWindowDimensions } from "react-native";
 import { TouchableOpacity } from "react-native-gesture-handler";
 import { useSelector } from "react-redux";
 import { RootState } from "@/app/redux/store";
-import { Ionicons } from '@expo/vector-icons';
+import { Ionicons } from "@expo/vector-icons";
 import { AccountInfo } from "@/constants";
+import { Skeleton } from "moti/skeleton";
 
 export default function AccountPageProfile() {
   const username = useSelector((state: RootState) => state.username.username);
-  
+  const [isLoading, setIsLoading] = useState(true);
+  const colorMode: "light" | "dark" = "light";
+
+  useEffect(() => {
+    if (typeof username === "string" && AccountInfo !== null) {
+      setIsLoading(false);
+    }
+  }, [username]);
+
   return (
     <View className="flex-row items-center justify-between w-11/12 my-6 bg-ocean-blue rounded-3xl p-3.5 pr-6 self-center">
       <TouchableOpacity className="flex-row items-center">
-      <Image source={AccountInfo[0].image} className="mr-1"/>
+        <Image source={AccountInfo[0].image} className="mr-1" />
         <View>
-          <Text className="text-base text-white">
-            {username ? username.charAt(0).toUpperCase() + username.slice(1) : 'Anonymous'}
-          </Text>
-          <Text className="text-slate-grey text-xs">
-            {AccountInfo[0].number}
-          </Text>
+          <Skeleton
+            colorMode={colorMode}
+            width={150} 
+            height={20}
+          >
+            {isLoading ? (
+              null
+            ) : (
+              <Text className="text-base text-white">
+                {username && username.charAt(0).toUpperCase() + username.slice(1)}
+              </Text>
+            )}
+          </Skeleton>
+          <Skeleton
+            colorMode={colorMode} 
+            width={150} 
+            height={20}
+          >
+            {isLoading ? null : (
+              <Text className="text-slate-grey text-xs">
+                {AccountInfo[0].number}
+              </Text>
+            )}
+          </Skeleton>
         </View>
       </TouchableOpacity>
       <TouchableOpacity>
-        <Ionicons name="settings-sharp" size={35} color="white"/>
+        <Ionicons name="settings-sharp" size={35} color="white" />
       </TouchableOpacity>
     </View>
-  )
+  );
 }
