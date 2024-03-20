ad12b1394203298586ed99dc266f80dcee50b9e4
Fam Awad <fam@awadlouis.com>
2024-03-20 21:04:15 +0100

    Added distance matrix status checking

--- server/app/que/src/main/java/com/que/que/Maps/MapsService.java
+++ server/app/que/src/main/java/com/que/que/Maps/MapsService.java
@@ -2,6 +2,7 @@ package com.que.que.Maps;
 
 import com.google.maps.GeoApiContext;
 import com.google.maps.model.DistanceMatrix;
+import com.google.maps.model.DistanceMatrixElementStatus;
 import com.google.maps.DistanceMatrixApi;
 
 public class MapsService {
@@ -15,8 +16,13 @@ public class MapsService {
         } catch (Exception e) {
             throw new IllegalStateException(e.toString());
         }
-        if (distanceMatrix == null) {
+        if (distanceMatrix == null || distanceMatrix.rows.length < 1) {
             throw new IllegalStateException("Could not get data");
+        }
+        if ((distanceMatrix.rows[0].elements[0].status).equals(DistanceMatrixElementStatus.NOT_FOUND)) {
+            throw new IllegalStateException("Locations were not found");
+        } else if ((distanceMatrix.rows[0].elements[0].status).equals(DistanceMatrixElementStatus.ZERO_RESULTS)) {
+            throw new IllegalStateException("No results were found");
         }
         String timeMin = distanceMatrix.rows[0].elements[0].duration.humanReadable;
         String timeMax = distanceMatrix.rows[0].elements[0].durationInTraffic.humanReadable;
