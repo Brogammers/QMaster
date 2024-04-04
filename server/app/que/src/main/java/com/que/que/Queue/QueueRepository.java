96e9d6cc43da9ad8fc82e55480d0f4df6b6b205b
Fam Awad <fam@awadlouis.com>
2024-04-04 20:05:53 +0200

    Removing unused imports

--- server/app/que/src/main/java/com/que/que/Queue/QueueRepository.java
+++ server/app/que/src/main/java/com/que/que/Queue/QueueRepository.java
@@ -1,6 +1,5 @@
 package com.que.que.Queue;
 
-import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.Optional;
 
@@ -14,43 +13,43 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface QueueRepository extends JpaRepository<Queues, Long> {
 
-    Optional<Queues> findByName(String name);
+        Optional<Queues> findByName(String name);
 
-    ArrayList<Queues> findByQueueSlotAndSpecificSlot(int queueSlot, int specificSlot);
+        ArrayList<Queues> findByQueueSlotAndSpecificSlot(int queueSlot, int specificSlot);
 
-    Page<Queues> findAll(Pageable page);
+        Page<Queues> findAll(Pageable page);
 
-    ArrayList<Queues> findByQueueSlot(int queueSlot);
+        ArrayList<Queues> findByQueueSlot(int queueSlot);
 
-    Page<Queues> findByNameContaining(String name, Pageable page);
+        Page<Queues> findByNameContaining(String name, Pageable page);
 
-    @Query("SELECT q FROM Queues q ORDER BY q.rating ASC")
-    Page<Queues> findAllOrderedByRatingAsc(Pageable page);
+        @Query("SELECT q FROM Queues q ORDER BY q.rating ASC")
+        Page<Queues> findAllOrderedByRatingAsc(Pageable page);
 
-    @Query("SELECT q FROM Queues q ORDER BY q.rating DESC")
-    Page<Queues> findAllOrderedByRatingDesc(Pageable page);
+        @Query("SELECT q FROM Queues q ORDER BY q.rating DESC")
+        Page<Queues> findAllOrderedByRatingDesc(Pageable page);
 
-    @Query("SELECT q FROM Queues q ORDER BY q.peopleInQueue ASC")
-    Page<Queues> findAllOrderedByPeopleInQueueAsc(Pageable page);
+        @Query("SELECT q FROM Queues q ORDER BY q.peopleInQueue ASC")
+        Page<Queues> findAllOrderedByPeopleInQueueAsc(Pageable page);
 
-    @Query("SELECT q FROM Queues q ORDER BY q.peopleInQueue DESC")
-    Page<Queues> findAllOrderedByPeopleInQueueDesc(Pageable page);
+        @Query("SELECT q FROM Queues q ORDER BY q.peopleInQueue DESC")
+        Page<Queues> findAllOrderedByPeopleInQueueDesc(Pageable page);
 
-    @Query("SELECT q FROM Queues q WHERE LOWER(q.name) LIKE LOWER(concat('%', :targetName, '%')) ORDER BY q.rating ASC")
-    Page<Queues> findAllOrderedByRatingAscAndFilterName(
-            @Param("targetName") String targetName, Pageable page);
+        @Query("SELECT q FROM Queues q WHERE LOWER(q.name) LIKE LOWER(concat('%', :targetName, '%')) ORDER BY q.rating ASC")
+        Page<Queues> findAllOrderedByRatingAscAndFilterName(
+                        @Param("targetName") String targetName, Pageable page);
 
-    @Query("SELECT q FROM Queues q WHERE LOWER(q.name) LIKE LOWER(concat('%', :targetName, '%')) ORDER BY q.rating DESC")
-    Page<Queues> findAllOrderedByRatingDescAndFilterName(
-            @Param("targetName") String targetName, Pageable page);
+        @Query("SELECT q FROM Queues q WHERE LOWER(q.name) LIKE LOWER(concat('%', :targetName, '%')) ORDER BY q.rating DESC")
+        Page<Queues> findAllOrderedByRatingDescAndFilterName(
+                        @Param("targetName") String targetName, Pageable page);
 
-    @Query("SELECT q FROM Queues q WHERE LOWER(q.name) LIKE LOWER(concat('%', :targetName, '%')) ORDER BY q.peopleInQueue ASC")
-    Page<Queues> findAllOrderedByPeopleInQueueAscAndFilterName(
-            @Param("targetName") String targetName, Pageable page);
+        @Query("SELECT q FROM Queues q WHERE LOWER(q.name) LIKE LOWER(concat('%', :targetName, '%')) ORDER BY q.peopleInQueue ASC")
+        Page<Queues> findAllOrderedByPeopleInQueueAscAndFilterName(
+                        @Param("targetName") String targetName, Pageable page);
 
-    @Query("SELECT q FROM Queues q WHERE LOWER(q.name) LIKE LOWER(concat('%', :targetName, '%')) ORDER BY q.peopleInQueue DESC")
-    Page<Queues> findAllOrderedByPeopleInQueueDescAndFilterName(
-            @Param("targetName") String targetName, Pageable page);
+        @Query("SELECT q FROM Queues q WHERE LOWER(q.name) LIKE LOWER(concat('%', :targetName, '%')) ORDER BY q.peopleInQueue DESC")
+        Page<Queues> findAllOrderedByPeopleInQueueDescAndFilterName(
+                        @Param("targetName") String targetName, Pageable page);
 
-    ArrayList<Queues> findByNameContaining(String name);
+        ArrayList<Queues> findByNameContaining(String name);
 }
