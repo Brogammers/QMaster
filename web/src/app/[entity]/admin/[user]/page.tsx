c4d1415951f112a30865b723a05e2fb30aab0458
Hatem Soliman <hatemthedev@gmail.com>
2024-04-10 09:20:00 +0200

    Installed the CoreUI library and implemented basic functionalitu

--- web/src/app/[entity]/admin/[user]/page.tsx
+++ web/src/app/[entity]/admin/[user]/page.tsx
@@ -1,6 +1,9 @@
+import Sidebar from "@/app/shared/Sidebar";
+
 export default function User() {
   return (
     <>
+      <h1>Hello arkness</h1>
     </>
   )
 }
--- web/src/app/[entity]/page.tsx
+++ web/src/app/[entity]/page.tsx
@@ -1,5 +1,17 @@
-export default function Entity() {
+"use client"
+
+import { ReactNode } from "react";
+import Sidebar from "../shared/Sidebar";
+
+interface EntityProps {
+  children: ReactNode;
+}
+
+export default function Entity({ children }: EntityProps) {
   return (
-    <></>
+    <>
+      <Sidebar />
+      {/* <main>{children}</main> */}
+    </>
   )
 }
