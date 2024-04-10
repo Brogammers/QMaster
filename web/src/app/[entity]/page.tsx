b5408221d794cf065bf285550d367f006edd96af
Hatem Soliman <hatemthedev@gmail.com>
2024-04-10 23:22:31 +0200

    Adjsuted routes

--- web/src/app/[entity]/page.tsx
+++ web/src/app/[entity]/page.tsx
@@ -1,15 +1,13 @@
 "use client"
 
 import { ChildrenProps } from "../../../types";
-import { Menu, Layout } from "antd";
-import Image from "next/image";
+import { Layout } from "antd";
 import { useState } from "react";
 import { useParams, useRouter } from "next/navigation";
-import { SettingOutlined, DesktopOutlined, UserOutlined } from '@ant-design/icons'; // Import required icons
-import QMasterLogo from "../../../public/qmaster.svg";
+import Sidebar from "../shared/Sidebar";
 
-const { Sider, Content } = Layout;
-const { SubMenu } = Menu;
+const { Content } = Layout;
+
 
 export default function Entity({ children }: ChildrenProps) {
   const { entity } = useParams();
@@ -25,35 +23,11 @@ export default function Entity({ children }: ChildrenProps) {
   };
 
   return (
-    <Layout style={{ minHeight: "100vh" }}>
-      <Sider collapsible collapsed={isCollapsed} onCollapse={toggleCollapsed} width={isCollapsed ? 80 : 200}>
-        <div className="px-2 py-4 flex items-center justify-center border-b-2 border-coal-black">
-          <Image
-            src={QMasterLogo}
-            alt="QMaster"
-            width={40}
-          />
-          {!isCollapsed && <span className="font-bold text-2xl ml-4">QMaster</span>}
-        </div>
-        <Menu theme="dark" defaultSelectedKeys={["1"]} mode="inline">
-          <SubMenu key="1" title="Admin" icon={<SettingOutlined />}>
-            <Menu.Item key="3" onClick={() => handleClick('/sharing-info')}>Sharing Info</Menu.Item>
-            <Menu.Item key="4" onClick={() => handleClick('/queues')}>Queues</Menu.Item>
-            <Menu.Item key="5" onClick={() => handleClick('/details')}>Details</Menu.Item>
-            <Menu.Item key="6" onClick={() => handleClick('/customer-feedback')}>Customer Feedback</Menu.Item>
-          </SubMenu>
-          <Menu.Item key="2" icon={<UserOutlined />} onClick={() => handleClick('/counter')}>
-            <span>Counter</span>
-          </Menu.Item>
-          <Menu.Item key="3" icon={<DesktopOutlined />} onClick={() => handleClick('/display')}>
-            <span>Display</span>
-          </Menu.Item>
-        </Menu>
-      </Sider>
-
+    <Layout className="min-h-screen">
+      <Sidebar />
       {/* Main Content */}
-      <Layout className="site-layout">
-        <Content style={{ margin: "0 16px" }}>
+      <Layout className="site-layout bg-coal-black text-white">
+        <Content className="text-white mx-4">
           <div className="site-layout-background" style={{ padding: 24, minHeight: 360 }}>
             <h2>{entity}&apos;s Coworking Space</h2>
             {/* Main content */}
--- web/src/app/shared/Sidebar.tsx
+++ web/src/app/shared/Sidebar.tsx
@@ -1,4 +1,6 @@
 import { useState, useEffect} from "react";
+import { useParams, useRouter } from "next/navigation";
+import Image from 'next/image';
 import { 
   CAlert, 
   CBadge,
@@ -14,13 +16,28 @@ import {
 import '@coreui/coreui/dist/css/coreui.min.css';
 import CIcon from '@coreui/icons-react';
 import { cilSettings, cilScreenDesktop, cilPeople } from '@coreui/icons';
-import { useParams } from "next/navigation";
-import Image from 'next/image';
+
 import QMasterLogo from "../../../public/qmaster.svg";
+import { ChildrenProps } from "../../../types";
+import { Menu, Layout } from "antd";
+import { SettingOutlined, DesktopOutlined, UserOutlined } from '@ant-design/icons'; // Import required icons
+
+const { Sider } = Layout;
+const { SubMenu } = Menu;
 
 
 export default function Sidebar() {
   const { entity } = useParams();
+  const router = useRouter();
+  const [isCollapsed, setIsCollapsed] = useState(true);
+
+  const toggleCollapsed = () => {
+    setIsCollapsed(!isCollapsed);
+  };
+
+  const handleClick = (route: string) => {
+    router.push(route);
+  };
 
   useEffect(() => {
     // Fetch the user's role from a server when they log in
@@ -30,65 +47,127 @@ export default function Sidebar() {
   }, []);
 
   return (
-    <CSidebar className="sidebar border-end h-screen" colorScheme="dark" unfoldable>
-      <CSidebarHeader className="border-bottom">
-        <CSidebarBrand className='font-bold no-underline flex items-center'>
-          <Image
-            src={QMasterLogo} 
-            alt='QMaster' 
-            width={40}
-          />
-          <span className='text__branding text-2xl ml-4'>QMaster</span>
-        </CSidebarBrand>
-      </CSidebarHeader>
-      <CSidebarNav>
-        <CNavTitle>Welcome back, Hatem</CNavTitle>
-        <CNavGroup
-          toggler={
-            <>
-              <CIcon customClassName="nav-icon" icon={cilSettings} /> Admin
-            </>
-          }
+    <Sider 
+      collapsible 
+      collapsed={isCollapsed} 
+      onCollapse={toggleCollapsed}
+    >
+      <div className="px-2 py-4 flex items-center justify-center border-b-2 border-coal-black">
+        <Image
+          src={QMasterLogo}
+          alt="QMaster"
+          width={40}
+        />
+        {!isCollapsed && 
+          <span className="font-bold text-white text-2xl ml-4">QMaster</span>
+        }
+      </div>
+      <Menu theme="dark" defaultSelectedKeys={["1"]} mode="inline">
+        <SubMenu 
+          key="1" 
+          title="Admin" 
+          icon={<SettingOutlined />}
         >
-          <CNavItem href={`/${entity}/admin/details`}>
-            <span className="nav-icon">
-              <span className="nav-icon-bullet" />
-            </span>
-            Details
-          </CNavItem>
-          <CNavItem href={`/${entity}/admin/queues`}>
-            <span className="nav-icon">
-              <span className="nav-icon-bullet" />
-            </span>
+          <Menu.Item 
+            key="3" 
+            onClick={() => handleClick('/sharing-info')}
+          >
+            Sharing Info
+          </Menu.Item>
+          <Menu.Item 
+            key="4" 
+            onClick={() => handleClick('/queues')}
+          >
             Queues
-          </CNavItem>
-          <CNavItem href={`/${entity}/admin/customer-feedback`}>
-            <span className="nav-icon">
-              <span className="nav-icon-bullet" />
-            </span>
+          </Menu.Item>
+          <Menu.Item 
+            key="5" 
+            onClick={() => handleClick('/details')}
+          >
+            Details
+          </Menu.Item>
+          <Menu.Item 
+            key="6" 
+            onClick={() => handleClick('/customer-feedback')}
+          >
             Customer Feedback
-          </CNavItem>
-        </CNavGroup>
-        <CNavItem href={`/${entity}/counter`}>
-          <CIcon 
-            customClassName="nav-icon" 
-            icon={cilPeople} 
-          />
-            Counter
-        </CNavItem>
-        <CNavItem href={`/${entity}/display`}>
-          <CIcon 
-            customClassName="nav-icon" 
-            icon={cilScreenDesktop}
-          />
-            Display
-            <CBadge color="primary ms-auto">NEW</CBadge>
-        </CNavItem>
-      </CSidebarNav>
-      <CSidebarHeader className="border-top">
-        <CSidebarToggler />
-      </CSidebarHeader>
-    </CSidebar>
+          </Menu.Item>
+        </SubMenu>
+        <Menu.Item 
+          key="2" 
+          icon={<UserOutlined />} 
+          onClick={() => handleClick('/counter')}
+        >
+          <span>Counter</span>
+        </Menu.Item>
+        <Menu.Item 
+          key="3" 
+          icon={<DesktopOutlined />} 
+          onClick={() => handleClick('/display')}
+        >
+          <span>Display</span>
+        </Menu.Item>
+      </Menu>
+    </Sider>
+    // <CSidebar className="sidebar border-end h-screen" colorScheme="dark" unfoldable>
+    //   <CSidebarHeader className="border-bottom">
+    //     <CSidebarBrand className='font-bold no-underline flex items-center'>
+    //       <Image
+    //         src={QMasterLogo} 
+    //         alt='QMaster' 
+    //         width={40}
+    //       />
+    //       <span className='text__branding text-2xl ml-4'>QMaster</span>
+    //     </CSidebarBrand>
+    //   </CSidebarHeader>
+    //   <CSidebarNav>
+    //     <CNavTitle>Welcome back, Hatem</CNavTitle>
+    //     <CNavGroup
+    //       toggler={
+    //         <>
+    //           <CIcon customClassName="nav-icon" icon={cilSettings} /> Admin
+    //         </>
+    //       }
+    //     >
+    //       <CNavItem href={`/${entity}/admin/details`}>
+    //         <span className="nav-icon">
+    //           <span className="nav-icon-bullet" />
+    //         </span>
+    //         Details
+    //       </CNavItem>
+    //       <CNavItem href={`/${entity}/admin/queues`}>
+    //         <span className="nav-icon">
+    //           <span className="nav-icon-bullet" />
+    //         </span>
+    //         Queues
+    //       </CNavItem>
+    //       <CNavItem href={`/${entity}/admin/customer-feedback`}>
+    //         <span className="nav-icon">
+    //           <span className="nav-icon-bullet" />
+    //         </span>
+    //         Customer Feedback
+    //       </CNavItem>
+    //     </CNavGroup>
+    //     <CNavItem href={`/${entity}/counter`}>
+    //       <CIcon 
+    //         customClassName="nav-icon" 
+    //         icon={cilPeople} 
+    //       />
+    //         Counter
+    //     </CNavItem>
+    //     <CNavItem href={`/${entity}/display`}>
+    //       <CIcon 
+    //         customClassName="nav-icon" 
+    //         icon={cilScreenDesktop}
+    //       />
+    //         Display
+    //         <CBadge color="primary ms-auto">NEW</CBadge>
+    //     </CNavItem>
+    //   </CSidebarNav>
+    //   <CSidebarHeader className="border-top">
+    //     <CSidebarToggler />
+    //   </CSidebarHeader>
+    // </CSidebar>
   )
 }
 
