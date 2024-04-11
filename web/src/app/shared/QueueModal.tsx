1868ee4a209f124f5360075e02def004ed57a67e
Hatem Soliman <hatemthedev@gmail.com>
2024-04-11 13:13:28 +0200

    Updated styling for the description prop

--- web/src/app/shared/QueueModal.tsx
+++ web/src/app/shared/QueueModal.tsx
@@ -4,7 +4,7 @@ export default function QueueModal({ title, description, children }: QueueModalP
   return (
     <div className="bg-white text-coal-black p-4 rounded-lg">
       <h1 className="text-2xl font-bold mb-4">{ title }</h1>
-      <p>{ description }</p>
+      <p className="mb-8">{ description }</p>
       {children}
     </div>
   );
