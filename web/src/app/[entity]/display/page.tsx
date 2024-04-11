afd48125b183d545d45cdf103582556b81fd2b00
Hatem Soliman <hatemthedev@gmail.com>
2024-04-12 01:38:04 +0200

    Developed the display page

--- web/src/app/[entity]/display/page.tsx
+++ web/src/app/[entity]/display/page.tsx
@@ -1,9 +1,26 @@
+import QueueModal from "@/app/shared/QueueModal";
 import Entity from "../page";
+import Image from "next/image";
+import DisplayImg from "../../../../public/display.svg";
+
 
 export default function Display() {
   return (
     <Entity>
-      <h1>This is the display page</h1>
+      <QueueModal>
+        <Image
+          src={DisplayImg}
+          alt="Display Image"
+          width={100}
+        />
+        <h1>Display View</h1>
+        <p>
+          You have the option to set up a display screen to present the current queue status to your visitors. Simply access this page using the computer connected to the display screen and activate fullscreen mode by clicking the designated button.
+        </p>
+        <button>
+          Fullscreen
+        </button>
+      </QueueModal>
     </Entity>
   )
 };
