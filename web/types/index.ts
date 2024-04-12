01cec200fc56ab05f85994ed6de66cd4507ca603
Hatem Soliman <hatemthedev@gmail.com>
2024-04-12 02:47:26 +0200

    Added interface and file

--- web/types/index.ts
+++ web/types/index.ts
@@ -5,3 +5,15 @@ export interface QueueModalProps {
   description?: string;
   children: ReactNode;
 }
+
+export interface TextButtonProps {
+  text: string;
+  buttonColor: string;
+  textColor: string;
+  textSize?: string;
+  width?: number;
+  padding?: number;
+  icon?: any;
+  onPress?: () => void;
+  disabled?: boolean;
+}
