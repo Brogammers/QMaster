ff3867791ed923ef81c3e54ca1d5838940d151db
Hatem Soliman <hatemthedev@gmail.com>
2024-04-12 02:30:26 +0200

    Added capitalize function and route to the title of page

--- web/src/app/[entity]/page.tsx
+++ web/src/app/[entity]/page.tsx
@@ -2,18 +2,32 @@
 
 import { QueueModalProps } from "../../../types";
 import { Layout } from "antd";
-import { useParams } from "next/navigation";
+import { useParams, usePathname } from "next/navigation";
 import Sidebar from "../shared/Sidebar";
 
 const { Content } = Layout;
 
 
 export default function Entity({ children }: QueueModalProps) {
+  const pathname = usePathname();
   let { entity } = useParams();
-  if (Array.isArray(entity)) {
-    entity = entity[0];
+  let page: string | undefined = '';
+
+  const capitalize = (str: string) => str.charAt(0).toUpperCase() + str.slice(1);
+
+  if (typeof entity === 'string') {
+    const segments = entity.split('/');
+    page = segments.pop();
   }
-  entity = entity.charAt(0).toUpperCase() + entity.slice(1);
+
+  if (page) {
+    const segments = pathname.split('/'); 
+    page = capitalize(segments[segments.length - 1]); 
+  }
+
+  if (typeof entity === 'string') {
+    entity = capitalize(entity);
+  }
 
   return (
     <Layout className="w-full min-h-screen">
@@ -22,7 +36,7 @@ export default function Entity({ children }: QueueModalProps) {
         <Content className="container w-full text-white mx-4">
           <div className="row site-layout-background">
             <h2 className="text-xl border-b-2 border-b-slight-slate-grey mb-4 py-3">
-              {entity}&apos;s Coworking Space
+              {entity}&apos;s Coworking Space &gt; {page}
             </h2>
             {children}
           </div>
