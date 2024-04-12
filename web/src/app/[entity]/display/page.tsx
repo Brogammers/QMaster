77fbf18af22258d324676f68b97370efe96b3cd0
Hatem Soliman <hatemthedev@gmail.com>
2024-04-12 02:36:15 +0200

    Added more styling

--- web/src/app/[entity]/display/page.tsx
+++ web/src/app/[entity]/display/page.tsx
@@ -8,18 +8,20 @@ export default function Display() {
   return (
     <Entity>
       <QueueModal>
-        <Image
-          src={DisplayImg}
-          alt="Display Image"
-          width={100}
-        />
-        <h1>Display View</h1>
-        <p>
-          You have the option to set up a display screen to present the current queue status to your visitors. Simply access this page using the computer connected to the display screen and activate fullscreen mode by clicking the designated button.
-        </p>
-        <button>
-          Fullscreen
-        </button>
+        <div className="px-56 text-center flex flex-col justify-center items-center">
+          <Image
+            src={DisplayImg}
+            alt="Display Image"
+            width={250}
+          />
+          <h1 className="text-3xl font-bold">Display View</h1>
+          <p className="text-lg">
+            You have the option to set up a display screen to present the current queue status to your visitors. Simply access this page using the computer connected to the display screen and activate fullscreen mode by clicking the designated button.
+          </p>
+          <button className="bg-baby-blue text-lg px-4 py-2 rounded-lg text-white font-bold">
+            Fullscreen
+          </button>
+        </div>
       </QueueModal>
     </Entity>
   )
