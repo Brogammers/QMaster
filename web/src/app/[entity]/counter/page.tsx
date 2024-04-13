f41f9bba33ff0af67431eb8124ce470e0037b511
Hatem Soliman <hatemthedev@gmail.com>
2024-04-13 19:44:30 +0200

    Installed MUI Tabs library

--- web/src/app/[entity]/counter/page.tsx
+++ web/src/app/[entity]/counter/page.tsx
@@ -1,14 +1,15 @@
 "use client"
 
 import React, { useEffect, useMemo, useState } from 'react';
-import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
-import 'react-tabs/style/react-tabs.css'; // Import styles for React Tabs
+import Box from '@mui/material/Box';
+import { Tab } from '@mui/material';
+import { TabPanel, TabList, TabContext } from '@mui/lab';
 import TicketNumber from '@/app/shared/TicketNumber';
 import Entity from '../page';
 
 export default function Counter() {
-  const [activeTab1, setActiveTab1] = useState<number>(0);
-  const [activeTab2, setActiveTab2] = useState<number>(0);
+  const [activeTab1, setActiveTab1] = useState<string>('0');
+  const [activeTab2, setActiveTab2] = useState<string>('0');
   const [visibleTickets1, setVisibleTickets1] = useState<any[]>([]);
   const [visibleTickets2, setVisibleTickets2] = useState<any[]>([]);
 
@@ -50,103 +51,125 @@ export default function Counter() {
     calculateVisibleTickets(tickets2, setVisibleTickets2);
   }, [tickets1, tickets2]);
 
+  const handleChange = (event: React.SyntheticEvent, newValue: string) => {
+    setActiveTab1(newValue);
+    setActiveTab2(newValue);
+  };
+
+  const filterTickets = (tickets: any[], tabValue: string) => {
+    if (tabValue === '0') {
+      return tickets; // return all tickets
+    } else if (tabValue === '1') {
+      return tickets.filter(ticket => ticket.service === 'Customer Service');
+    } else if (tabValue === '2') {
+      return tickets.filter(ticket => ticket.service === 'New Customer');
+    }
+    return [];
+  };
+
   return (
     <Entity>
       <div className="flex flex-col justify-start gap-16">
         <div>
           <h3>Serving</h3>
-          <Tabs width={window.innerWidth}>
-            <TabList>
-              {/* Tabs navigation for first row */}
-              <Tab onClick={() => setActiveTab1(0)}>All servings</Tab>
-              <Tab onClick={() => setActiveTab1(1)}>Customer Service</Tab>
-              <Tab onClick={() => setActiveTab1(2)}>New Customer</Tab>
-              {/* Add more tabs as needed */}
-            </TabList>
+          <Box sx={{ width: '100%', typography: 'body1' }}>
+            <TabContext value={activeTab1}>
+              <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+                <TabList onChange={handleChange} aria-label="lab API tabs example" sx={{ color: 'white' }}>
+                  <Tab label="All servings" value="0" />
+                  <Tab label="Customer Service" value="1" />
+                  <Tab label="New Customer" value="2" />
+                  {/* Add more tabs as needed */}
+                </TabList>
+              </Box>
 
-            {/* Tab panels for first row */}
-            <TabPanel className="w-full">
-              {/* Scrollable container for first row */}
-              <div className="w-full overflow-x-scroll flex">
-                {/* Render list of ticket/cards for the selected service */}
-                {tickets1.map((ticket, index) => (
-                  <TicketNumber 
-                    key={ticket.id} 
-                    active={index === activeTab1}
-                    bgColor="white"
-                    textColor="black"
-                    fontSize="3xl"
-                    borderRadius="md"
-                    width="6"
-                    queue={ticket.service}
-                    ticketNum={ticket.ticketNumber}
-                  />
-                ))}
-                {/* Display overflow ticket if needed */}
-                {tickets1.length > MAX_TICKETS && (
-                  <TicketNumber
-                    key="overflow"
-                    ticketNum={`${tickets1.length - MAX_TICKETS}+`}
-                    bgColor="white"
-                    textColor="black"
-                    fontSize="lg"
-                    borderRadius="md"
-                    width="6"
-                  />
-                )}
-              </div>
-            </TabPanel>
+              {/* Tab panels for first row */}
+              <TabPanel value="0">
+                {/* Scrollable container for first row */}
+                <div className="w-full overflow-x-scroll flex">
+                  {/* Render list of ticket/cards for the selected service */}
+                  {filterTickets(tickets1, activeTab1).map((ticket, index) => (
+                    <TicketNumber 
+                      key={ticket.id} 
+                      active={index === parseInt(activeTab1)}
+                      bgColor="white"
+                      textColor="black"
+                      fontSize="3xl"
+                      borderRadius="md"
+                      width="6"
+                      queue={ticket.service}
+                      ticketNum={ticket.ticketNumber}
+                    />
+                  ))}
+                  {/* Display overflow ticket if needed */}
+                  {tickets1.length > MAX_TICKETS && (
+                    <TicketNumber
+                      key="overflow"
+                      ticketNum={`${tickets1.length - MAX_TICKETS}+`}
+                      bgColor="white"
+                      textColor="black"
+                      fontSize="lg"
+                      borderRadius="md"
+                      width="6"
+                    />
+                  )}
+                </div>
+              </TabPanel>
 
-            {/* Add more TabPanels as needed for additional services */}
-          </Tabs>
+              {/* Add more TabPanels as needed for additional services */}
+            </TabContext>
+          </Box>
         </div>
 
         <div>
           <h3>In waiting line</h3>
-          <Tabs>
-            <TabList className="border-b-slate-grey border-b-2 flex items-center">
-              {/* Tabs navigation for second row */}
-              <Tab selectedClassName='bg-transparent' onClick={() => setActiveTab2(0)}>All queues</Tab>
-              <Tab selectedClassName='bg-transparent' onClick={() => setActiveTab2(1)}>Customer Service</Tab>
-              <Tab selectedClassName='bg-transparent' onClick={() => setActiveTab2(2)}>New Customer</Tab>
-              {/* Add more tabs as needed */}
-            </TabList>
+          <Box sx={{ width: '100%', typography: 'body1' }}>
+            <TabContext value={activeTab2}>
+              <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+                <TabList onChange={handleChange} aria-label="lab API tabs example" sx={{ color: 'white' }}>
+                  <Tab label="All queues" value="0" />
+                  <Tab label="Customer Service" value="1" />
+                  <Tab label="New Customer" value="2" />
+                  {/* Add more tabs as needed */}
+                </TabList>
+              </Box>
 
-            {/* Tab panels for second row */}
-            <TabPanel>
-              {/* Scrollable container for second row */}
-              <div className="w-full overflow-x-scroll flex">
-                {/* Render list of ticket/cards for the selected service */}
-                {tickets2.map((ticket, index) => (
-                  <TicketNumber 
-                    key={ticket.id} 
-                    active={index === activeTab2}
-                    bgColor="white"
-                    textColor="black"
-                    fontSize="3xl"
-                    borderRadius="md"
-                    width="6"
-                    queue={ticket.service}
-                    ticketNum={ticket.ticketNumber}
-                  />
-                ))}
-                {/* Display overflow ticket if needed */}
-                {tickets2.length > MAX_TICKETS && (
-                  <TicketNumber
-                    key="overflow"
-                    ticketNum={`${tickets2.length - MAX_TICKETS}+`}
-                    bgColor="white"
-                    textColor="black"
-                    fontSize="lg"
-                    borderRadius="md"
-                    width="6"
-                  />
-                )}
-              </div>
-            </TabPanel>
+              {/* Tab panels for second row */}
+              <TabPanel value="0">
+                {/* Scrollable container for second row */}
+                <div className="w-full overflow-x-scroll flex">
+                  {/* Render list of ticket/cards for the selected service */}
+                  {filterTickets(tickets2, activeTab2).map((ticket, index) => (
+                    <TicketNumber 
+                      key={ticket.id} 
+                      active={index === parseInt(activeTab2)}
+                      bgColor="white"
+                      textColor="black"
+                      fontSize="3xl"
+                      borderRadius="md"
+                      width="6"
+                      queue={ticket.service}
+                      ticketNum={ticket.ticketNumber}
+                    />
+                  ))}
+                  {/* Display overflow ticket if needed */}
+                  {tickets2.length > MAX_TICKETS && (
+                    <TicketNumber
+                      key="overflow"
+                      ticketNum={`${tickets2.length - MAX_TICKETS}+`}
+                      bgColor="white"
+                      textColor="black"
+                      fontSize="lg"
+                      borderRadius="md"
+                      width="6"
+                    />
+                  )}
+                </div>
+              </TabPanel>
 
-            {/* Add more TabPanels as needed for additional services */}
-          </Tabs>
+              {/* Add more TabPanels as needed for additional services */}
+            </TabContext>
+          </Box>
         </div>
       </div>
     </Entity>
