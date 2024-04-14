c445cc5ff9b5b9a06ac6769deed01af87e738cc0
Fam Awad <fam@awadlouis.com>
2024-04-14 18:00:55 +0200

    Changing depricated functions

--- server/app/que/src/test/java/com/que/que/Registration/RegistrationServiceTest.java
+++ server/app/que/src/test/java/com/que/que/Registration/RegistrationServiceTest.java
@@ -35,7 +35,7 @@ public class RegistrationServiceTest {
 
     @Before
     public void setUp() {
-        MockitoAnnotations.initMocks(this);
+        MockitoAnnotations.openMocks(this);
         registrationService = new RegistrationService(
                 appUserService,
                 emailValidator,
