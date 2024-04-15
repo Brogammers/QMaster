f845ba3d5936d83cb6fdbc0fd16221131dd10576
Hatem Soliman <hatemthedev@gmail.com>
2024-04-15 23:29:14 +0200

    Added new updates and fixes

--- web/src/app/shared/TicketNumber.tsx
+++ web/src/app/shared/TicketNumber.tsx
@@ -8,26 +8,11 @@ export default function TicketNumber({ bgColor, textColor, fontSize, borderRadiu
   return (
     <div 
       className={`border-ocean-blue border-dashed border-2 bg-${bgColor} text-${textColor} rounded-${borderRadius} text-center font-bold ${width && `w-${width}`} ${maxWidth && `max-w-${maxWidth}`} h-6} px-0 py-8 flex flex-col justify-center items-center gap-4`}
-      onMouseEnter={() => setShowButton(true)}
-      onMouseLeave={() => setShowButton(false)}
     >
       <span className={`text-${fontSize} mx-16 whitespace-nowrap`}>
         { ticketNum }
       </span>
-      {queue && (
-        <span className={`text-sm bg-baby-blue text-white rounded-md px-2 py-1 ${labelPadding && `px-${labelPadding}`}`}>
-          { queue }
-        </span>
-      )}
-      {onClick && showButton && (
-        <TextButton
-          text={queue === "others waiting in queue" ? "Reject" : "Done"} // Dynamic button text based on queue
-          buttonColor={queue === "others waiting in queue" ? "red" : "green"} // Dynamic button color based on queue
-          textColor="white"
-          textSize="sm"
-          onPress={onClick} // Call onClick function on button press
-        />
-      )}
+      {queue && <span className={`text-sm bg-baby-blue text-white rounded-md px-2 py-1 ${labelPadding && `px-${labelPadding}`}`}>{ queue }</span>}
     </div>
   );
 };
