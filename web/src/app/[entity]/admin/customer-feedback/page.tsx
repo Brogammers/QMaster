ae3fc988dfac188bb87e8993b9dd49b0a733e312
Hatem Soliman <hatemthedev@gmail.com>
2024-04-20 22:42:51 +0200

    Added tag to customer feedback page

--- web/src/app/[entity]/admin/customer-feedback/page.tsx
+++ web/src/app/[entity]/admin/customer-feedback/page.tsx
@@ -1,10 +1,26 @@
+import Image from "next/image";
 import Entity from "../../page";
+import UnderConstruction from "../../../../../public/under-construction.svg";
+import Tag from "@/app/shared/Tag";
 
 
 export default function CustimerFeedback() {
   return (
     <Entity>
-      <h1>This is the customer feedback page</h1>
+      <div className="my-32 flex flex-col justify-center items-center gap-8">
+        <figure className=" w-1/2 rounded-3xl">
+          <Image
+            src={UnderConstruction}
+            alt="Under Construction"
+            width={1024}
+          />
+        </figure>
+        <Tag
+          text="this page is under construction"
+          bgColor="red-500"
+          fontSize="xl"
+        />
+      </div>
     </Entity>
   )
 }
--- web/src/app/shared/Tag.tsx
+++ web/src/app/shared/Tag.tsx
@@ -2,7 +2,7 @@ import { TagProps } from "../../../types";
 
 export default function Tag({ text, bgColor, fontSize }: TagProps) {
   return (
-    <span className={`bg-${bgColor} px-2 py-1 rounded-md font-bold text-center text-white text-${fontSize}`}>
+    <span className={`bg-${bgColor ? bgColor : `red-500`} px-2 py-1 rounded-md font-bold text-center text-white text-${fontSize}`}>
       {text}
     </span>
   )
