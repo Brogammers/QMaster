cd83749c6094719a65d52226a506a449edad041a
Hatem Soliman <hatemthedev@gmail.com>
2024-04-26 21:35:45 +0300

    Animation started

--- web/src/app/[entity]/display/page.tsx
+++ web/src/app/[entity]/display/page.tsx
@@ -1,13 +1,13 @@
 "use client"
 
-import QueueModal from "@/app/shared/QueueModal";
-import Entity from "../page";
-import Image from "next/image";
-import DisplayImg from "../../../../public/display.svg";
-import TeamImg from "../../../../public/team.jpg";
 import { useEffect, useState } from "react";
 import useWindowSize from "../../../../hooks/useWindowSize";
 import TicketNumber from "@/app/shared/TicketNumber";
+import Image from "next/image";
+import TeamImg from "../../../../public/team.jpg";
+import DisplayImg from "../../../../public/display.svg";
+import QueueModal from "@/app/shared/QueueModal";
+import Entity from "../page";
 
 // Sample queued persons data
 const queuedPersonsData = [
@@ -25,9 +25,10 @@ const queuedPersonsData = [
 export default function Display() {
   const [fullscreen, setFullscreen] = useState<boolean>(false);
   const [queuedPersons, setQueuedPersons] = useState(queuedPersonsData);
+  const [scrollIndex, setScrollIndex] = useState(0);
+  const [isScrolling, setIsScrolling] = useState(false);
 
-  const width = useWindowSize().width * .75;
-  const height = useWindowSize().height;
+  const { width, height } = useWindowSize();
 
   useEffect(() => {
     const handleKeyPress = (event: KeyboardEvent) => {
@@ -42,66 +43,80 @@ export default function Display() {
     document.addEventListener('fullscreenchange', handleFullscreenChange);
 
     return () => {
-      document.addEventListener('keydown', handleKeyPress);
-      document.addEventListener('fullscreenchange', handleFullscreenChange);
+      document.removeEventListener('keydown', handleKeyPress);
+      document.removeEventListener('fullscreenchange', handleFullscreenChange);
     };
   }, [fullscreen]);
 
   const handleFullscreen = () => {
     setFullscreen(!fullscreen);
-  }
+  };
 
   const maxTicketsToShow = Math.floor(height / 164);
+
+  useEffect(() => {
+    if (queuedPersonsData.length > maxTicketsToShow) {
+      const scrollInterval = setInterval(() => {
+        setIsScrolling(true);
+        setScrollIndex((prevIndex) => (prevIndex + 1) % (queuedPersonsData.length - maxTicketsToShow + 1));
+      }, 3000); // Adjust scroll speed as needed
+
+      return () => clearInterval(scrollInterval);
+    }
+  }, [maxTicketsToShow]);
 
   return (
     <>
       {fullscreen ? (
-        <div className="h-screen w-full flex justify-between">
+        <div className="h-screen w-full flex justify-between overflow-hidden">
           <Image
             src={TeamImg}
             alt="QMaster"
-            width={width}
+            width={width * 0.75}
             height={height}
           />
-          <div className="w-1/4 bg-ocean-blue">
-            {queuedPersons.slice(0, maxTicketsToShow).map((person, index) => (
-              <TicketNumber
-                key={person.id}
-                bgColor={index % 2 === 0 ? `white` : `transparent`}
-                textColor={index % 2 === 0 ? `black` : `white`}
-                fontSize="3xl"
-                borderRadius="none"
-                width="full"
-                maxWidth="16"
-                ticketNum={person.ticketNumber}
-                queue={person.counter}
-              />
-            ))}
-          </div>
+          {queuedPersonsData.length > maxTicketsToShow && isScrolling && (
+            <div className="w-1/4 bg-ocean-blue flex flex-col">
+              {queuedPersons.slice(scrollIndex, scrollIndex + maxTicketsToShow).map((person, index) => (
+                <TicketNumber
+                  key={person.id}
+                  bgColor={index % 2 === 0 ? `white` : `transparent`}
+                  textColor={index % 2 === 0 ? `black` : `white`}
+                  fontSize="3xl"
+                  borderRadius="none"
+                  width="full"
+                  maxWidth="16"
+                  ticketNum={person.ticketNumber}
+                  queue={person.counter}
+                />
+              ))}
+            </div>
+          )}
         </div>
       ) : (
-      <Entity>
-        <QueueModal width="w-full">
-          <div className="px-56 text-center flex flex-col justify-center items-center gap-8">
-            <Image
-              src={DisplayImg}
-              alt="Display Image"
-              width={200}
-            />
-            <h1 className="text-3xl font-bold">Display View</h1>
-            <p className="text-lg">
-              You have the option to set up a display screen to present the current queue status to your visitors. Simply access this page using the computer connected to the display screen and activate fullscreen mode by clicking the designated button.
-            </p>
-            <button 
-              onClick={handleFullscreen}
-              className="bg-baby-blue px-4 py-2 rounded-lg text-white text-lg font-bold"
-            >
-              Fullscreen
-            </button>
-          </div>
-        </QueueModal>
-      </Entity>
+        <Entity>
+          <QueueModal width="w-full">
+            <div className="px-56 text-center flex flex-col justify-center items-center gap-8">
+              <Image
+                src={DisplayImg}
+                alt="Display Image"
+                width={200}
+              />
+              <h1 className="text-3xl font-bold">Display View</h1>
+              <p className="text-lg">
+                You have the option to set up a display screen to present the current queue status to your visitors. Simply access this page using the computer connected to the display screen and activate fullscreen mode by clicking the designated button.
+              </p>
+              <button
+                onClick={handleFullscreen}
+                className="bg-baby-blue px-4 py-2 rounded-lg text-white text-lg font-bold"
+              >
+                Fullscreen
+              </button>
+            </div>
+          </QueueModal>
+        </Entity>
       )}
     </>
   )
 };
+
