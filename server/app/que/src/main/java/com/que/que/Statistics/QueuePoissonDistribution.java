77486a5637319637c3c96262ff98bf2dfdd92f1f
Fam Awad <fam@awadlouis.com>
2024-04-30 21:10:32 +0200

    Adding Statistics unit tests

---
+++ server/app/que/src/main/java/com/que/que/Statistics/QueuePoissonDistribution.java
@@ -0,0 +1,26 @@
+package com.que.que.Statistics;
+
+public class QueuePoissonDistribution {
+
+    /**
+     * Method to calculate the probability of a user joining the queue.
+     * 
+     * @param lambda The average number of users joining the queue per unit time.
+     * @param k      The number of users joining the queue.
+     * @return The probability of k users joining the queue.
+     */
+    public double calculateProbability(double lambda, int k) {
+
+    }
+
+    /**
+     * Calculates the factorial of a given integer.
+     *
+     * @param x the integer for which the factorial is to be calculated
+     * @return the factorial of the given integer
+     */
+    // Note: This method should not be implemented recursively
+    public int factorial(int x) {
+
+    }
+}
