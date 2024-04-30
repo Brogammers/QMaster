324a769eb7da2321799721d87a4a9aacda2026fd
Hatem Soliman <hatemthedev@gmail.com>
2024-04-30 20:10:59 +0300

    Added successully the loading animation to Login screen

--- web/src/app/login/page.tsx
+++ web/src/app/login/page.tsx
@@ -1,11 +1,21 @@
+"use client"
+
+import { useState } from "react";
 import LoginForm from "../components/LoginForm";
+import SplashScreen from "../shared/SplashScreen";
 
 export default function Login() {
+  const [isLoading, setIsLoading] = useState(false);
+
+  if (isLoading) {
+    return <SplashScreen />;
+  }
+
   return (
     <div className="bg-animation flex flex-col justify-center items-center">
       <div className="login__form container row text-center flex flex-col justify-center items-center h-4/5">
         <h2 className="login__form--title text-8xl font-bold text-white mb-16">Login</h2>
-        <LoginForm />
+        <LoginForm setIsLoading={setIsLoading} />
       </div>
     </div>
   )
