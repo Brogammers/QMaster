669539d5cfd987213f4f27023410ef7d66150d3d
Hatem Soliman <hatemthedev@gmail.com>
2024-04-30 19:27:17 +0300

    Temp resolved errors

--- web/src/app/shared/SplashScreen.tsx
+++ web/src/app/shared/SplashScreen.tsx
@@ -1,30 +1,24 @@
-import { useEffect } from 'react';
-import { Player, Controls } from '@lottiefiles/react-lottie-player';
-import Lottie from 'react-lottie';
-import Logo from '../../../public/splash.json';
 
-export default function SplashScreen({ additionalText, backgroundColor = '#17222D' }) {
+
+export default function SplashScreen() {
   const defaultOptions = {
     loop: true,
     autoplay: true,
-    animationData: animationData,
+    // animationData: animationData,
     rendererSettings: {
       preserveAspectRatio: "xMidYMid slice"
     }
   };
 
   return (
-    <div id="splash-screen" className="flex items-center justify-center min-h-screen bg-ocean-blue" style={{ backgroundColor }}>
+    <div id="splash-screen" className="flex items-center justify-center min-h-screen bg-ocean-blue">
       <div className="w-1/2 h-1/2">
-      <Lottie 
+      {/* <Lottie 
         options={defaultOptions}
         height={400}
         width={400}
-      />
+      /> */}
       </div>
-      {additionalText && (
-        <p className="text-white text-center mt-6">{additionalText}</p>
-      )}
     </div>
   );
 }
