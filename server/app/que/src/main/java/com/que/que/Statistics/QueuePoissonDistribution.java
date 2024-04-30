667d650d89e22f1f3f159eb5a501ddbf400b5bf3
Fam Awad <fam@awadlouis.com>
2024-04-30 21:29:04 +0200

    Adding overrided functions

--- server/app/que/src/main/java/com/que/que/Statistics/QueuePoissonDistribution.java
+++ server/app/que/src/main/java/com/que/que/Statistics/QueuePoissonDistribution.java
@@ -2,4 +2,16 @@ package com.que.que.Statistics;
 
 public class QueuePoissonDistribution extends PoissonStatisticalModel {
 
+    @Override
+    public double calculateProbability(double lambda, int k) {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public int factorial(int x) {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
 }
