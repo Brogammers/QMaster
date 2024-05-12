5c5c09810ba63cfe2385095ac1448d5d7d7797c4
Fam Awad <fam@awadlouis.com>
2024-05-12 21:30:47 +0200

    Added BusinessUser class and its serializer

--- server/app/que/src/main/java/com/que/que/User/BusinessUser.java
+++ server/app/que/src/main/java/com/que/que/User/BusinessUser.java
@@ -1,7 +1,20 @@
 package com.que.que.User;
 
-import jakarta.persistence.Column;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Setter
+@Getter
+@NoArgsConstructor
+@EqualsAndHashCode(callSuper = true)
+@JsonSerialize(using = BusinessUserSerializer.class)
 public class BusinessUser extends User {
     @Column(nullable = false)
     private int queueId; // If queue id is -1 then there is no queue
---
+++ server/app/que/src/main/java/com/que/que/User/BusinessUserSerializer.java
@@ -0,0 +1,30 @@
+package com.que.que.User;
+
+import java.io.IOException;
+
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.JsonSerializer;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import com.fasterxml.jackson.databind.ser.std.StdSerializer;
+
+public class BusinessUserSerializer extends StdSerializer<BusinessUser> {
+
+    public BusinessUserSerializer() {
+        this(null);
+    }
+
+    public BusinessUserSerializer(Class<BusinessUser> t) {
+        super(t);
+    }
+
+    @Override
+    public void serialize(
+            BusinessUser value, JsonGenerator jgen, SerializerProvider provider) throws IOException {
+
+        jgen.writeStartObject();
+        jgen.writeStringField("first-name", value.getFirstName());
+        jgen.writeStringField("last-name", value.getLastName());
+        jgen.writeStringField("username", value.getUsername());
+
+    }
+}
