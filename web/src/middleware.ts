6119e8a10a4bdebeb3b0ee75b4b41a06cf9cc996
Hatem Soliman <hatemthedev@gmail.com>
2024-06-18 16:49:10 +0300

    Removed redundant files

--- web/src/middleware.ts
+++
@@ -1,6 +0,0 @@
-import { NextRequest } from "next/server";
-import { updateSession } from "./lib";
-
-export async function middleware(request: NextRequest) {
-  return await updateSession(request);
-}
