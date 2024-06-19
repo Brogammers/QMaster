7c213c4ad772e3e089732b538a15a9a919dbf9f3
Hatem Soliman <hatemthedev@gmail.com>
2024-06-19 04:03:05 +0300

    Font bolder for perks titles

--- web/src/app/components/Perks.tsx
+++ web/src/app/components/Perks.tsx
@@ -8,7 +8,7 @@ export default function Perks() {
       <div className="container">
         <div className="w-full flex items-start mb-96">
           <div className="row">
-            <h3 className="text-8xl w-1/2 leading-normal">
+            <h3 className="text-8xl w-1/2 leading-normal font-bold">
               Powerful Account
             </h3>
           </div>
@@ -25,7 +25,7 @@ export default function Perks() {
             className="absolute left-0"
           />
           <div className="row">
-            <h3 className="pl-16 text-8xl w-1/2 float-right leading-normal">
+            <h3 className="pl-16 text-8xl w-1/2 float-right leading-normal font-bold">
               Seamless Queuing
             </h3>
           </div>
