216592d3e97965b616c00c5fff5088ef416cfa25
Hatem Soliman <hatemthedev@gmail.com>
2024-06-22 00:30:24 +0300

    Support made responsive

--- web/src/app/components/Support.tsx
+++ web/src/app/components/Support.tsx
@@ -19,9 +19,9 @@ export default function Support() {
     <section data-aos="fade-left" className="double__color--btn py-4">
       <div className="container">
         <div className="row">
-          <div data-aos="fade-right" data-aos-duration="800" data-aos-offset="600" className="w-full flex justify-between items-start gap-24">
-            <div className="w-1/2 flex flex-col gap-8">
-              <h3 className="text-5xl font-bold leading-normal">
+          <div data-aos="fade-right" data-aos-duration="800" data-aos-offset="600" className="w-full flex flex-col sm:flex-row items-center sm:justify-between sm:items-start gap-24">
+            <div className="w-full sm:w-1/2 flex flex-col gap-8">
+              <h3 className="text-7xl sm:text-5xl font-bold leading-normal">
                 Need Assistance?
                 <br />
                 We&apos;re Here to Help! 
@@ -33,7 +33,7 @@ export default function Support() {
                 If you have any questions or need help, you can contact us directly through the feedback feature within our app under the &quot;Account&quot; section, or email us directly at <Link href="mailto:support@qmaster.com">support@qmaster.com</Link>. We&apos;ll respond within 24 hours to make your day easier.
               </p>
             </div>
-            <figure className="w-1/2">
+            <figure className="w-full sm:w-1/2">
               <Image
                 src={SupportImg}
                 alt="Contact Support Images"
