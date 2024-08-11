4ee83ff548a4d882b3db4eafaacc3b0f895200cd
FamALouiz <fam@awadlouis.com>
2024-08-11 20:06:30 +0200

    Updated proxy to dynamic proxy

--- web/next.config.mjs
+++ web/next.config.mjs
@@ -3,8 +3,8 @@ const nextConfig = {
   async rewrites() {
     return [
       {
-        source: "/api/v1/login/business",
-        destination: "http://localhost:8080/api/v1/login/business",
+        source: "/api/:path*",
+        destination: "http://localhost:8080/api/:path*",
       },
     ];
   },
