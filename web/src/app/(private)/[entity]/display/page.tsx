be90466a38d1475c71d440a9774610b08a9138e8
FamALouiz <fam@awadlouis.com>
2024-08-11 18:01:34 +0200

    Updated imports and handleLogin function

--- web/src/app/(private)/[entity]/display/page.tsx
+++ web/src/app/(private)/[entity]/display/page.tsx
@@ -1,29 +1,29 @@
-"use client"
+"use client";
 
 import { useEffect, useState } from "react";
-import useWindowSize from "../../../../hooks/useWindowSize";
+import useWindowSize from "../../../../../hooks/useWindowSize";
 import TicketNumber from "@/app/shared/TicketNumber";
 import Image from "next/image";
-import TeamImg from "../../../../public/team.jpg";
-import DisplayImg from "../../../../public/display.svg";
+import TeamImg from "../../../../../public/team.jpg";
+import DisplayImg from "../../../../../public/display.svg";
 import QueueModal from "@/app/shared/QueueModal";
 import Entity from "../page";
-import { gsap } from 'gsap';
-import { ScrollTrigger } from 'gsap/ScrollTrigger';
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 
 gsap.registerPlugin(ScrollTrigger);
 
 // Sample queued persons data
 const queuedPersonsData = [
-  { id: 1, ticketNumber: 'C-123', counter: 'counter 1' },
-  { id: 2, ticketNumber: 'C-124', counter: 'counter 6' },
-  { id: 3, ticketNumber: 'C-125', counter: 'counter 3' },
-  { id: 4, ticketNumber: 'C-016', counter: 'counter 4' },
-  { id: 5, ticketNumber: 'C-127', counter: 'counter 13' },
-  { id: 6, ticketNumber: 'P-127', counter: 'counter 16' },
-  { id: 7, ticketNumber: 'A-007', counter: 'counter 9' },
-  { id: 8, ticketNumber: 'B-037', counter: 'counter 2' },
-  { id: 9, ticketNumber: 'O-127', counter: 'counter 5' },
+  { id: 1, ticketNumber: "C-123", counter: "counter 1" },
+  { id: 2, ticketNumber: "C-124", counter: "counter 6" },
+  { id: 3, ticketNumber: "C-125", counter: "counter 3" },
+  { id: 4, ticketNumber: "C-016", counter: "counter 4" },
+  { id: 5, ticketNumber: "C-127", counter: "counter 13" },
+  { id: 6, ticketNumber: "P-127", counter: "counter 16" },
+  { id: 7, ticketNumber: "A-007", counter: "counter 9" },
+  { id: 8, ticketNumber: "B-037", counter: "counter 2" },
+  { id: 9, ticketNumber: "O-127", counter: "counter 5" },
 ];
 
 export default function Display() {
@@ -40,12 +40,12 @@ export default function Display() {
       setFullscreen(!document.fullscreenElement);
     };
 
-    document.addEventListener('keydown', handleKeyPress);
-    document.addEventListener('fullscreenchange', handleFullscreenChange);
+    document.addEventListener("keydown", handleKeyPress);
+    document.addEventListener("fullscreenchange", handleFullscreenChange);
 
     return () => {
-      document.removeEventListener('keydown', handleKeyPress);
-      document.removeEventListener('fullscreenchange', handleFullscreenChange);
+      document.removeEventListener("keydown", handleKeyPress);
+      document.removeEventListener("fullscreenchange", handleFullscreenChange);
     };
   }, [fullscreen]);
 
@@ -64,7 +64,7 @@ export default function Display() {
           setTimeout(() => {
             gsap.set(".scrollContainer", { y: 0 }); // Reset to start position
           }, 2000); // Wait for 2 seconds before restarting
-        }
+        },
       });
     }
   }, [fullscreen]);
@@ -99,14 +99,13 @@ export default function Display() {
         <Entity>
           <QueueModal width="w-full">
             <div className="px-56 text-center flex flex-col justify-center items-center gap-8">
-              <Image
-                src={DisplayImg}
-                alt="Display Image"
-                width={200}
-              />
+              <Image src={DisplayImg} alt="Display Image" width={200} />
               <h1 className="text-3xl font-bold">Display View</h1>
               <p className="text-lg">
-                You have the option to set up a display screen to present the current queue status to your visitors. Simply access this page using the computer connected to the display screen and activate fullscreen mode by clicking the designated button.
+                You have the option to set up a display screen to present the
+                current queue status to your visitors. Simply access this page
+                using the computer connected to the display screen and activate
+                fullscreen mode by clicking the designated button.
               </p>
               <button
                 onClick={handleFullscreen}
@@ -119,7 +118,5 @@ export default function Display() {
         </Entity>
       )}
     </>
-  )
-};
-
-
+  );
+}
--- web/src/app/(private)/[entity]/page.tsx
+++ web/src/app/(private)/[entity]/page.tsx
@@ -1,37 +1,36 @@
-"use client"
+"use client";
 
 import { QueueModalProps } from "../../../../types";
 import { Layout } from "antd";
 import { useParams, usePathname } from "next/navigation";
 import Sidebar from "@/app/shared/Sidebar";
 
-
 export default function Entity({ children }: QueueModalProps) {
   const pathname = usePathname();
   let { entity } = useParams();
-  let page: string | undefined = '';
+  let page: string | undefined = "";
 
   const formatPageName = (name: string) => {
-    const formattedName = name.replace(/[^\w\s]/gi, ' ');
-  
-    const capitalizeWord = (str: string) => str.charAt(0).toUpperCase() + str.slice(1);
-    const capitalizedWords = formattedName.split(' ').map(capitalizeWord);
-  
-    return capitalizedWords.join(' ');
-  };
-  
+    const formattedName = name.replace(/[^\w\s]/gi, " ");
 
-  if (typeof entity === 'string') {
-    const segments = entity.split('/');
+    const capitalizeWord = (str: string) =>
+      str.charAt(0).toUpperCase() + str.slice(1);
+    const capitalizedWords = formattedName.split(" ").map(capitalizeWord);
+
+    return capitalizedWords.join(" ");
+  };
+
+  if (typeof entity === "string") {
+    const segments = entity.split("/");
     page = segments.pop();
   }
 
   if (page) {
-    const segments = pathname.split('/'); 
-    page = formatPageName(segments[segments.length - 1]); 
+    const segments = pathname.split("/");
+    page = formatPageName(segments[segments.length - 1]);
   }
 
-  if (typeof entity === 'string') {
+  if (typeof entity === "string") {
     entity = formatPageName(entity);
   }
 
