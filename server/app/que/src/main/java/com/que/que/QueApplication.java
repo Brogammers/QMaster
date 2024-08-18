a977c3d9417eeeab23461184708f2f28dea1b0a8
FamALouiz <fam@awadlouis.com>
2024-08-18 22:05:35 +0200

    Updated qrcode retrieval and enqueueing

--- server/app/que/src/main/java/com/que/que/QueApplication.java
+++ server/app/que/src/main/java/com/que/que/QueApplication.java
@@ -1,13 +1,29 @@
 package com.que.que;
 
+import java.util.concurrent.TimeUnit;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.CacheControl;
+import org.springframework.lang.NonNull;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-public class QueApplication {
+public class QueApplication implements WebMvcConfigurer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(QueApplication.class, args);
+	}
+
+	@Override
+	public void addResourceHandlers(@NonNull ResourceHandlerRegistry registry) {
+
+		// Register resource handler for images
+		registry.addResourceHandler("/images/**").addResourceLocations("/WEB-INF/images/")
+				.setCacheControl(CacheControl.maxAge(2, TimeUnit.HOURS).cachePublic());
 	}
 
 }
