12f340f6a46bbc0c7b8f4b0abbb3dffc02c87ded
Mohammed Elazab <mohammedelazab16@gmail.com>
2024-08-25 07:53:31 +0300

    added support for right to left languages

--- mobile-app/i18n.ts
+++ mobile-app/i18n.ts
@@ -1,7 +1,7 @@
 import { I18n } from "i18n-js";
-//import en from "./locales/en.json";
 import ar from "./locales/ar.json";
 import en from './locales/en.json';
+import {getLocales} from 'expo-localization';
 
 const i18n = new I18n({
  en: en,
@@ -9,6 +9,6 @@ const i18n = new I18n({
 });
 
 i18n.enableFallback = true;
-i18n.locale = "ar";
+i18n.locale =  getLocales()[0].languageCode?.toString() || 'ar';
 
 export default i18n;
