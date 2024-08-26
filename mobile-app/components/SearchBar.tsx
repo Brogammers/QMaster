6974d9fb807a6e0cbcdd43f388526a11c0d720b1
Hatem Soliman <hatemthedev@gmail.com>
2024-08-26 22:41:06 +0300

    Styled the SearchBar to in nativewind to reduce code and added RTL

--- mobile-app/components/SearchBar.tsx
+++ mobile-app/components/SearchBar.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet } from 'react-native';
+import { I18nManager, StyleSheet } from 'react-native';
 import { TouchableOpacity } from 'react-native-gesture-handler';
 import { View, Text } from 'react-native';
 import { FontAwesome } from '@expo/vector-icons';
@@ -15,26 +15,13 @@ export default function SearchBar() {
 
   return (
     <TouchableOpacity
-      style={styles.searchBar}
+      className="h-11 border-2 rounded-3xl border-rock-stone mt-7 mb-4 px-4 py-2 flex justify-center"
       onPress={handleSearchPress}
     >
-      <View style={{ flexDirection: 'row', alignItems: 'center' }}>
-        <FontAwesome name="search" size={24} color="black" />
+      <View className={`${I18nManager.isRTL ? `flex-row-reverse justify-between` : `flex-row`} items-center`}>
+        <FontAwesome name="search" size={24} color="black" className={`${I18nManager.isRTL && `pl-4`}`} />
         <Text style={{ marginLeft: 10 }}>{i18n.t('search')}</Text>
       </View>
     </TouchableOpacity>
   );
 }
-
-const styles = StyleSheet.create({
-  searchBar: {
-    height: 44,
-    borderWidth: 2,
-    borderColor: '#969696',
-    borderRadius: 20,
-    paddingLeft: 15,
-    justifyContent: 'center',
-    marginTop: 28,
-    marginBottom: 16,
-  },
-});
