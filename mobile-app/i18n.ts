4d92eb57f86671c8ce983c4f6c7aa572b19a23b1
Mohammed Elazab <mohammedelazab16@gmail.com>
2024-08-26 10:39:05 +0300

    language gets set based on location

--- mobile-app/i18n.ts
+++ mobile-app/i18n.ts
@@ -10,6 +10,6 @@ const i18n = new I18n({
 
 i18n.enableFallback = true;
 i18n.locale =  getLocales()[0].languageCode?.toString() || 'ar';
-i18n.locale =  'ar';
+//i18n.locale =  'ar';
 
 export default i18n;
