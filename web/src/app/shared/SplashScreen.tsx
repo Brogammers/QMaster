2bd9d4d711498a40a29bfda004dd28a857222507
Hatem Soliman <hatemthedev@gmail.com>
2025-01-10 07:21:48 +0200

    SplashScreen is splashing

--- web/src/app/shared/SplashScreen.tsx
+++ web/src/app/shared/SplashScreen.tsx
@@ -1,9 +1,11 @@
+'use client'
+
 import animatedLogo from "../../../public/splash.json";
 import Lottie from "lottie-react";
 
 export default function SplashScreen() {
   return (
-    <div className="absolute top-0 left-0 flex items-center justify-center w-full min-h-screen bg-ocean-blue">
+    <div className="fixed inset-0 z-[9999] flex items-center justify-center w-full h-full bg-ocean-blue">
       <Lottie animationData={animatedLogo} width={100} autoplay loop />
     </div>
   );
