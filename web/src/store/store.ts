24ee08afa6806ad9392aed1e6bba84e3f108ca78
Hatem Soliman <hatemthedev@gmail.com>
2025-01-10 18:39:00 +0200

    Fixed issue

--- web/src/store/store.ts
+++ web/src/store/store.ts
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import userReducer from './features/userSlice';
+import entityReducer from './features/entitySlice';
 
 export const store = configureStore({
   reducer: {
     users: userReducer,
+    entity: entityReducer,
   },
 });
 
