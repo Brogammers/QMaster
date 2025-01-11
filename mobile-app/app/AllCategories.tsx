74da5921116717c279d12b9c4ddd102f1a2c5dbc
Hatem Soliman <hatemthedev@gmail.com>
2025-01-11 23:43:10 +0200

    Added some

--- mobile-app/app/AllCategories.tsx
+++ mobile-app/app/AllCategories.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Text, ScrollView } from 'react-native';
+import { View, Text, ScrollView, SafeAreaView } from 'react-native';
 import { useLinkTo } from '@react-navigation/native';
 import Category from '@/shared/components/Category';
 import { AllCategories as CategoryList } from '@/constants';
@@ -18,22 +18,24 @@ export default function AllCategories() {
   };
 
   return (
-    <ScrollView className="flex-1 bg-off-white">
-      <View className="p-4">
-        <Text className="text-2xl font-bold mb-4">
-          {i18n.t('allCategories')}
-        </Text>
-        <View className="flex flex-row flex-wrap justify-between">
-          {CategoryList.map((category: CategoryProps, index: number) => (
-            <Category
-              key={index}
-              title={category.title}
-              image={category.image}
-              onPress={() => handleCategoryPress(category)}
-            />
-          ))}
+    <SafeAreaView>
+      <ScrollView className="flex-1 bg-off-white">
+        <View className="p-4">
+          <Text className="text-2xl font-bold mb-4">
+            {i18n.t('allCategories')}
+          </Text>
+          <View className="flex flex-row flex-wrap justify-between">
+            {CategoryList.map((category: CategoryProps, index: number) => (
+              <Category
+                key={index}
+                title={category.title}
+                image={category.image}
+                onPress={() => handleCategoryPress(category)}
+              />
+            ))}
+          </View>
         </View>
-      </View>
-    </ScrollView>
+      </ScrollView>
+    </SafeAreaView>
   );
 } 
