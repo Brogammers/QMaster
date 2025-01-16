ff2e157a091c18b06d26baf8d0103276eb02fb3d
Hatem Soliman <hatemthedev@gmail.com>
2025-01-16 04:35:06 +0200

    Transformed styling

--- web/src/app/components/store/ProductPreviewModal.tsx
+++ web/src/app/components/store/ProductPreviewModal.tsx
@@ -80,16 +80,9 @@ export default function ProductPreviewModal({
                 type="button"
                 variant="outline"
                 onClick={onClose}
-                className="border-2 border-baby-blue text-baby-blue hover:bg-baby-blue/5 bg-transparent mr-4"
+                className="border-2 border-baby-blue text-baby-blue hover:bg-baby-blue/5 bg-transparent"
               >
-                Cancel
-              </Button>
-              <Button
-                className="!bg-gradient-to-r !from-baby-blue !to-ocean-blue hover:!opacity-90 !text-white"
-                disabled={product.stock === 0}
-              >
-                <ShoppingCart className="w-4 h-4 mr-2" />
-                Add to Cart
+                Close
               </Button>
             </div>
           </div>
