8253038ce314bcfbaf225023d8aebc45857ca7d5
Hatem Soliman <hatemthedev@gmail.com>
2025-02-28 01:57:54 +0200

    Added the current queues list dynamically withe partner page

--- mobile-app/app/_layout.tsx
+++ mobile-app/app/_layout.tsx
@@ -1,23 +1,23 @@
-import React, { useEffect, useState } from 'react';
-import { Slot } from 'expo-router';
-import { SessionProvider } from '@/ctx/AuthContext';
-import { store } from './redux/store';
-import { Provider } from 'react-redux';
-import { useFonts } from 'expo-font';
-import SplashScreen from './SplashScreen';
-import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import React, { useEffect, useState } from "react";
+import { Slot } from "expo-router";
+import { SessionProvider } from "@/ctx/AuthContext";
+import { store } from "./redux/store";
+import { Provider } from "react-redux";
+import { useFonts } from "expo-font";
+import SplashScreen from "./SplashScreen";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
 import { ThemeProvider } from "@/ctx/ThemeContext";
-import { CartProvider } from '@/ctx/CartContext';
-import { SiaProvider } from '../ctx/SiaContext';
+import { CartProvider } from "@/ctx/CartContext";
+import { SiaProvider } from "../ctx/SiaContext";
 // REACT-NATIVE-KEYBOARD-CONTROLLER
 // import { KeyboardProvider } from 'react-native-keyboard-controller';
 
 export default function RootLayout() {
   const [loaded, error] = useFonts({
-    InterBold: require('../assets/fonts/static/Inter-Bold.ttf'),
-    JostBold: require('../assets/fonts/static/Jost-Bold.ttf'),
-    JostReg: require('../assets/fonts/static/Jost-Regular.ttf'),
-    IstokBold: require('../assets/fonts/static/IstokWeb-Bold.ttf'),
+    InterBold: require("../assets/fonts/static/Inter-Bold.ttf"),
+    JostBold: require("../assets/fonts/static/Jost-Bold.ttf"),
+    JostReg: require("../assets/fonts/static/Jost-Regular.ttf"),
+    IstokBold: require("../assets/fonts/static/IstokWeb-Bold.ttf"),
   });
 
   const [showLoading, setShowLoading] = useState(true);
@@ -41,13 +41,13 @@ export default function RootLayout() {
       <Provider store={store}>
         <ThemeProvider>
           {/* <KeyboardProvider> */}
-            <CartProvider>
-              <SessionProvider>
-                <SiaProvider>
-                  <Slot />
-                </SiaProvider>
-              </SessionProvider>
-            </CartProvider>
+          <CartProvider>
+            <SessionProvider>
+              <SiaProvider>
+                <Slot />
+              </SiaProvider>
+            </SessionProvider>
+          </CartProvider>
           {/* </KeyboardProvider> */}
         </ThemeProvider>
       </Provider>
