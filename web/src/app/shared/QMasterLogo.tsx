6ac0ac2706e4cb7b67e31da31f5aa416b5d32c63
Hatem Soliman <hatemthedev@gmail.com>
2025-03-04 16:14:46 +0200

    Unified the QMaster logo size and font

--- web/src/app/shared/QMasterLogo.tsx
+++ web/src/app/shared/QMasterLogo.tsx
@@ -5,14 +5,14 @@ import { QMasterLogoProps } from "types";
 export default function QMasterLogo({ className, displayQMasterText = false }: QMasterLogoProps) {
   return (
     <div className="flex items-center gap-5">
-      <figure className="w-[49.78px] sm:w-[200px">
+      <figure className="w-[56px]">
         <Image
           src={QMasterSVG}
           alt="QMaster Logo"
           className={`w-full ${className}`}
         />
       </figure>
-      {displayQMasterText && <span className="font-jost text-white text-3xl sm:block hidden">QMaster</span>}
+      {displayQMasterText && <span className="jost-font text-white text-4xl sm:block hidden">QMaster</span>}
     </div>
   );
 }
